d6f8b85a8bb859bbd09b209f6e33a5eafc512ec0
Giulia Campana <giuliaecampana@gmail.com>
2020-10-19 09:35:19 -0700

    ProfileContainer

---
+++ src/containers/profile/src/ProfileContainer.js
@@ -0,0 +1,42 @@
+// @flow
+import React from 'react';
+
+import styled from 'styled-components';
+import { StyleUtils } from 'lattice-ui-kit';
+
+import ProfileAside from './ProfileAside';
+
+import { CrumbItem, Crumbs } from '../../../components/crumbs';
+
+const { media } = StyleUtils;
+
+const ProfileGrid = styled.div`
+  display: grid;
+  grid-gap: 18px;
+  grid-template-columns: auto 1fr;
+  ${media.phone`
+    grid-template-columns: auto;
+  `}
+`;
+
+type Props = {
+  name ?:string;
+  personId :UUID;
+};
+
+const ProfileContainer = ({ name, personId } :Props) => (
+  <div>
+    <Crumbs>
+      <CrumbItem>{ name }</CrumbItem>
+    </Crumbs>
+    <ProfileGrid>
+      <ProfileAside personId={personId} />
+    </ProfileGrid>
+  </div>
+);
+
+ProfileContainer.defaultProps = {
+  name: ''
+};
+
+export default ProfileContainer;
