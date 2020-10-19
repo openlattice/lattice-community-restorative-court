5a3a143f2814f274169ce35c3fa13e32764879a6
Giulia Campana <giuliaecampana@gmail.com>
2020-10-19 08:59:20 -0700

    shared profile styled components

---
+++ src/containers/profile/src/styled/index.js
@@ -0,0 +1,20 @@
+// @flow
+import styled from 'styled-components';
+
+const Body = styled.div`
+  display: grid;
+  grid-gap: 36px;
+  grid-auto-flow: row;
+`;
+
+const CenterWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+`;
+
+export {
+  Body,
+  CenterWrapper
+};
