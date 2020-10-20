1f220c016727d6b7f5aeb262c635b6a1d648fa4b
Giulia Campana <giuliaecampana@gmail.com>
2020-10-20 15:10:54 -0700

    use useSelector

--- src/containers/app/AppSwitch.js
+++ src/containers/app/AppSwitch.js
@@ -2,14 +2,16 @@
 import React, { useEffect } from 'react';
 
 import { Spinner } from 'lattice-ui-kit';
+import { useRequestState } from 'lattice-utils';
 import { Route, Switch } from 'react-router-dom';
 import type { Match } from 'react-router';
+import type { RequestState } from 'redux-reqseq';
 
-import { useDispatch, useSelector } from './AppProvider';
-import { INITIALIZE_APPLICATION, initializeApplication } from './actions'
+import { useDispatch } from './AppProvider';
+import { INITIALIZE_APPLICATION, initializeApplication } from './actions';
 
 import ProfileContainer from '../profile/src/ProfileContainer';
-import { APP, REQUEST_STATE } from '../../core/redux/constants';
+import { APP } from '../../core/redux/constants';
 import { requestIsPending } from '../../utils/redux';
 import { CenterWrapper } from '../profile/src/styled';
 
@@ -33,7 +35,7 @@ const HelplineSwitch = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch, organizationId, root]);
 
-  const initializeRequestState = useSelector((state) => state.getIn([APP, INITIALIZE_APPLICATION, REQUEST_STATE]));
+  const initializeRequestState :?RequestState = useRequestState([APP, INITIALIZE_APPLICATION]);
   if (requestIsPending(initializeRequestState)) {
     return <CenterWrapper><Spinner size="3x" /></CenterWrapper>;
   }
