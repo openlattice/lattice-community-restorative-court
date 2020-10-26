f2866c982fd0ca5cf7936b98e00ff1bf20d32e51
Giulia Campana <giuliaecampana@gmail.com>
2020-10-26 09:44:48 -0700

    update selectors index

--- src/core/redux/selectors/index.js
+++ src/core/redux/selectors/index.js
@@ -3,4 +3,6 @@
  */
 
 export { default as selectEntitySetEntityType } from './selectEntitySetEntityType';
+export { default as selectEntitySetId } from './selectEntitySetId';
 export { default as selectEntitySetPropertyTypes } from './selectEntitySetPropertyTypes';
+export { default as selectFqn } from './selectFqn';
