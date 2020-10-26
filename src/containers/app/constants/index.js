ec378ffb53728059e285312bf472e606d16a420d
Giulia Campana <giuliaecampana@gmail.com>
2020-10-26 09:49:53 -0700

    app constants

---
+++ src/containers/app/constants/index.js
@@ -0,0 +1,18 @@
+// @flow
+
+const APP_PATHS = {
+  APP_CONFIG: ['app', 'appConfig', 'config'],
+  FQN: ['app', 'fqns'],
+  MATCH: ['app', 'match'],
+  ROOT: ['app', 'root'],
+};
+
+const NEIGHBOR_DIRECTIONS = {
+  DST: 'dst',
+  SRC: 'src',
+};
+
+export {
+  APP_PATHS,
+  NEIGHBOR_DIRECTIONS,
+};
