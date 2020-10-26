23469979067694ab7199486c38092e850e7fba69
Giulia Campana <giuliaecampana@gmail.com>
2020-10-26 09:48:14 -0700

    loadProfile saga

---
+++ src/containers/profile/src/sagas/loadProfile.js
@@ -0,0 +1,79 @@
+// @flow
+
+import {
+  all,
+  call,
+  put,
+  select,
+  takeEvery,
+} from '@redux-saga/core/effects';
+import { LangUtils, Logger } from 'lattice-utils';
+import type { Saga } from '@redux-saga/core';
+import type { SequenceAction } from 'redux-reqseq';
+
+import { getPersonNeighborsWorker } from './getPersonNeighbors';
+
+import { CASE, ROLE } from '../../../../core/redux/constants';
+import { selectEntitySetId } from '../../../../core/redux/selectors';
+import { ERR_ACTION_VALUE_NOT_DEFINED } from '../../../../utils/error/constants';
+import { NEIGHBOR_DIRECTIONS } from '../../../app/constants';
+import { LOAD_PROFILE, getPersonNeighbors, loadProfile } from '../actions';
+
+const { isDefined } = LangUtils;
+const { DST } = NEIGHBOR_DIRECTIONS;
+
+const LOG = new Logger('ProfileSagas');
+
+/*
+ *
+ * ProfileActions.loadProfile()
+ *
+ */
+
+function* loadProfileWorker(action :SequenceAction) :Saga<*> {
+
+  const { id } = action;
+  try {
+    yield put(loadProfile.request(id));
+    const { value } = action;
+    if (!isDefined(value)) throw ERR_ACTION_VALUE_NOT_DEFINED;
+
+    const personEKID :UUID = value;
+
+    const caseESID :UUID = yield select(selectEntitySetId(CASE));
+    const roleESID :UUID = yield select(selectEntitySetId(ROLE));
+
+    const neighbors = [
+      { direction: DST, entitySetId: caseESID },
+      { direction: DST, entitySetId: roleESID },
+    ];
+
+    const workerResponses :Object[] = yield all([
+      call(getPersonNeighborsWorker, getPersonNeighbors({ neighbors, personEKID })),
+    ]);
+    const responseError = workerResponses.reduce(
+      (error, workerResponse) => error || workerResponse.error,
+      undefined,
+    );
+    if (responseError) throw responseError;
+
+    yield put(loadProfile.success(id));
+  }
+  catch (error) {
+    LOG.error(action.type, error);
+    yield put(loadProfile.failure(id, error));
+  }
+  finally {
+    yield put(loadProfile.finally(id));
+  }
+}
+
+function* loadProfileWatcher() :Saga<*> {
+
+  yield takeEvery(LOAD_PROFILE, loadProfileWorker);
+}
+
+export {
+  loadProfileWatcher,
+  loadProfileWorker,
+};
