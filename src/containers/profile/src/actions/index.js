04f85dc3b9b87a269caa5b27a47672be1a0d604e
Giulia Campana <giuliaecampana@gmail.com>
2020-10-26 09:45:32 -0700

    profile actions

--- src/containers/profile/src/actions/index.js
+++ src/containers/profile/src/actions/index.js
@@ -3,10 +3,20 @@
 import { newRequestSequence } from 'redux-reqseq';
 import type { RequestSequence } from 'redux-reqseq';
 
-const GET_PERSON_CASES :'GET_PERSON_CASES' = 'GET_PERSON_CASES';
-const getPersonCases :RequestSequence = newRequestSequence(GET_PERSON_CASES);
+const GET_PERSON :'GET_PERSON' = 'GET_PERSON';
+const getPerson :RequestSequence = newRequestSequence(GET_PERSON);
+
+const GET_PERSON_NEIGHBORS :'GET_PERSON_NEIGHBORS' = 'GET_PERSON_NEIGHBORS';
+const getPersonNeighbors :RequestSequence = newRequestSequence(GET_PERSON_NEIGHBORS);
+
+const LOAD_PROFILE :'LOAD_PROFILE' = 'LOAD_PROFILE';
+const loadProfile :RequestSequence = newRequestSequence(LOAD_PROFILE);
 
 export {
-  GET_PERSON_CASES,
-  getPersonCases,
+  GET_PERSON,
+  GET_PERSON_NEIGHBORS,
+  LOAD_PROFILE,
+  getPerson,
+  getPersonNeighbors,
+  loadProfile,
 };
