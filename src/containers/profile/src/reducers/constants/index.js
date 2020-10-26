0fe944c7f52a2c9ef92307c1ef95bb34d0b518c5
Giulia Campana <giuliaecampana@gmail.com>
2020-10-26 09:49:00 -0700

    update profile redux constants

--- src/containers/profile/src/reducers/constants/index.js
+++ src/containers/profile/src/reducers/constants/index.js
@@ -2,10 +2,12 @@
 
 const PERSON = 'person';
 const PERSON_CASES = 'personCases';
+const PERSON_NEIGHBOR_MAP = 'personNeighborMap';
 const PROFILE = 'profile';
 
 export {
   PERSON,
   PERSON_CASES,
+  PERSON_NEIGHBOR_MAP,
   PROFILE,
 };
