0e0fbb54631e0f1af01d8c06eac70dd0e334b498
Giulia Campana <giuliaecampana@gmail.com>
2020-10-26 09:48:50 -0700

    update profile reducer

--- src/containers/profile/src/reducers/index.js
+++ src/containers/profile/src/reducers/index.js
@@ -1,27 +1,37 @@
 // @flow
 
-import { List, Map, fromJS } from 'immutable';
+import { Map, fromJS } from 'immutable';
 
-import getPersonCasesReducer from './getPersonCasesReducer';
-import { PERSON, PERSON_CASES } from './constants';
+import getPersonNeighborsReducer from './getPersonNeighborsReducer';
+import loadProfileReducer from './loadProfileReducer';
+import { PERSON, PERSON_NEIGHBOR_MAP } from './constants';
 
 import { RS_INITIAL_STATE } from '../../../../core/redux/constants';
-import { GET_PERSON_CASES, getPersonCases } from '../actions';
+import {
+  GET_PERSON_NEIGHBORS,
+  LOAD_PROFILE,
+  getPersonNeighbors,
+  loadProfile,
+} from '../actions';
 
 const INITIAL_STATE :Map = fromJS({
   // actions
-  [GET_PERSON_CASES]: RS_INITIAL_STATE,
+  [GET_PERSON_NEIGHBORS]: RS_INITIAL_STATE,
+  [LOAD_PROFILE]: RS_INITIAL_STATE,
   // data
   [PERSON]: Map(),
-  [PERSON_CASES]: List(),
+  [PERSON_NEIGHBOR_MAP]: Map(),
 });
 
 export default function reducer(state :Map<*, *> = INITIAL_STATE, action :Object) {
 
   switch (action.type) {
 
-    case getPersonCases.case(action.type):
-      return getPersonCasesReducer(state, action);
+    case getPersonNeighbors.case(action.type):
+      return getPersonNeighborsReducer(state, action);
+
+    case loadProfile.case(action.type):
+      return loadProfileReducer(state, action);
 
     default:
       return state;
