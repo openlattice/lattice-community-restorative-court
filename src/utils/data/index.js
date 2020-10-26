8ef6efcfa282bcc0b922c6419b88335b58c80f7e
Giulia Campana <giuliaecampana@gmail.com>
2020-10-26 09:53:14 -0700

    update data utils

--- src/utils/data/index.js
+++ src/utils/data/index.js
@@ -16,7 +16,13 @@ const getPropertyValues = (
   defaultValue ? :string = ''
 ) :any[] => fqns.map((fqn :FQN | string) => getPropertyValue(entity, fqn, defaultValue));
 
+const getNeighborDetails = (neighbor :Map | Object) :Map | Object => getIn(neighbor, 'neighborDetails');
+
+const getNeighborESID = (neighbor :Map | Object) :UUID => getIn(neighbor, ['neighborEntitySet', 'id']);
+
 export {
+  getNeighborDetails,
+  getNeighborESID,
   getPropertyValue,
   getPropertyValues,
 };
