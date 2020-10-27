61862121345799af48ec606051b3b92a6b00ba84
Giulia Campana <giuliaecampana@gmail.com>
2020-10-26 18:18:50 -0700

    add new profile redux constants

--- src/containers/profile/src/reducers/constants/index.js
+++ src/containers/profile/src/reducers/constants/index.js
@@ -1,13 +1,17 @@
 // @flow
 
+const PEOPLE_IN_CASE_BY_ROLE_EKID_MAP = 'peopleInCaseByRoleEKIDMap';
 const PERSON = 'person';
 const PERSON_CASES = 'personCases';
+const PERSON_CASE_NEIGHBOR_MAP = 'personCaseNeighborMap';
 const PERSON_NEIGHBOR_MAP = 'personNeighborMap';
 const PROFILE = 'profile';
 
 export {
+  PEOPLE_IN_CASE_BY_ROLE_EKID_MAP,
   PERSON,
   PERSON_CASES,
+  PERSON_CASE_NEIGHBOR_MAP,
   PERSON_NEIGHBOR_MAP,
   PROFILE,
 };
