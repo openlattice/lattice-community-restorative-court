6bcfae105f5c83699df5cb8e41c29e75ace52e13
Giulia Campana <giuliaecampana@gmail.com>
2020-10-28 00:18:53 -0700

    CaseStatusConstants

---
+++ src/containers/profile/src/constants/CaseStatusConstants.js
@@ -0,0 +1,8 @@
+// @flow
+
+export const ACCEPTANCE :string = 'Acceptance';
+export const CIRCLE :string = 'Circle';
+export const CLOSED :string = 'Closed';
+export const INTAKE :string = 'Intake';
+export const REFERRAL :string = 'Referral';
+export const RESOLUTION :string = 'Resolution';
