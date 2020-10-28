a450e4b5b77a5a56739a4b25cf8fe0f18d4ed306
Giulia Campana <giuliaecampana@gmail.com>
2020-10-27 17:38:17 -0700

    add new property types

--- src/core/edm/constants/PropertyTypes.js
+++ src/core/edm/constants/PropertyTypes.js
@@ -7,12 +7,15 @@ import { Models } from 'lattice';
 const { FQN } = Models;
 
 const PROPERTY_TYPE_FQNS = {
+  DATETIME_START: FQN.of('ol.datetimestart'),
+  DESCRIPTION: FQN.of('ol.description'),
   DOB: FQN.of('nc.PersonBirthDate'),
   EFFECTIVE_DATE: FQN.of('ol.effectivedate'),
   GIVEN_NAME: FQN.of('nc.PersonGivenName'),
   OL_DATE_TIME: FQN.of('ol.datetime'),
   OL_ID: FQN.of('ol.id'),
   SURNAME: FQN.of('nc.PersonSurName'),
+  TYPE: FQN.of('ol.type'),
 };
 
 export default PROPERTY_TYPE_FQNS;
