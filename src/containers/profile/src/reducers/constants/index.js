96ab70a62186d6b337a14b222b98f7ae215b1a7b
Giulia Campana <giuliaecampana@gmail.com>
2020-10-27 17:39:22 -0700

    more profile redux constants

--- src/containers/profile/src/reducers/constants/index.js
+++ src/containers/profile/src/reducers/constants/index.js
@@ -1,11 +1,12 @@
 // @flow
 
-const PEOPLE_IN_CASE_BY_ROLE_EKID_MAP = 'peopleInCaseByRoleEKIDMap';
-const PERSON = 'person';
-const PERSON_CASES = 'personCases';
-const PERSON_CASE_NEIGHBOR_MAP = 'personCaseNeighborMap';
-const PERSON_NEIGHBOR_MAP = 'personNeighborMap';
-const PROFILE = 'profile';
+const PEOPLE_IN_CASE_BY_ROLE_EKID_MAP :string = 'peopleInCaseByRoleEKIDMap';
+const PERSON :string = 'person';
+const PERSON_CASES :string = 'personCases';
+const PERSON_CASE_NEIGHBOR_MAP :string = 'personCaseNeighborMap';
+const PERSON_NEIGHBOR_MAP :string = 'personNeighborMap';
+const PERSON_ROLE_BY_CASE_EKID :string = 'personRoleByCaseEKID';
+const PROFILE :string = 'profile';
 
 export {
   PEOPLE_IN_CASE_BY_ROLE_EKID_MAP,
@@ -13,5 +14,6 @@ export {
   PERSON_CASES,
   PERSON_CASE_NEIGHBOR_MAP,
   PERSON_NEIGHBOR_MAP,
+  PERSON_ROLE_BY_CASE_EKID,
   PROFILE,
 };
