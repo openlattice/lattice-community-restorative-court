5cecd72694996abd22c700cd2f7fc509409898c6
Giulia Campana <giuliaecampana@gmail.com>
2020-10-28 00:16:14 -0700

    adjust getPersonCaseNeighborsReducer

--- src/containers/profile/src/reducers/getPersonCaseNeighborsReducer.js
+++ src/containers/profile/src/reducers/getPersonCaseNeighborsReducer.js
@@ -3,7 +3,12 @@ import { Map } from 'immutable';
 import { RequestStates } from 'redux-reqseq';
 import type { SequenceAction } from 'redux-reqseq';
 
-import { PEOPLE_IN_CASE_BY_ROLE_EKID_MAP, PERSON_CASE_NEIGHBOR_MAP, PERSON_ROLE_BY_CASE_EKID } from './constants';
+import {
+  PEOPLE_IN_CASE_BY_ROLE_EKID_MAP,
+  PERSON_CASE_NEIGHBOR_MAP,
+  PERSON_ROLE_BY_CASE_EKID,
+  STAFF_MEMBER_BY_STATUS_EKID,
+} from './constants';
 
 import { REQUEST_STATE } from '../../../../core/redux/constants';
 import { GET_PERSON_CASE_NEIGHBORS, getPersonCaseNeighbors } from '../actions';
@@ -18,6 +23,7 @@ export default function reducer(state :Map, action :SequenceAction) {
       .set(PEOPLE_IN_CASE_BY_ROLE_EKID_MAP, action.value.peopleInCaseByRoleEKIDMap)
       .set(PERSON_CASE_NEIGHBOR_MAP, action.value.personCaseNeighborMap)
       .set(PERSON_ROLE_BY_CASE_EKID, action.value.personRoleByCaseEKID)
+      .set(STAFF_MEMBER_BY_STATUS_EKID, action.value.staffMemberByStatusEKID)
       .setIn([GET_PERSON_CASE_NEIGHBORS, REQUEST_STATE], RequestStates.SUCCESS),
     FAILURE: () => state.setIn([GET_PERSON_CASE_NEIGHBORS, REQUEST_STATE], RequestStates.FAILURE),
   });
