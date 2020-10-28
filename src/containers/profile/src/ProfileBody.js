d9a3a0bf23e4354b3c3554f16c1e7e6df0842cfe
Giulia Campana <giuliaecampana@gmail.com>
2020-10-28 01:01:46 -0700

    include LastContacted in profile if person is ever a peacemaker

--- src/containers/profile/src/ProfileBody.js
+++ src/containers/profile/src/ProfileBody.js
@@ -2,11 +2,23 @@
 import React from 'react';
 
 import styled from 'styled-components';
+import { List, Map } from 'immutable';
 import { StyleUtils } from 'lattice-ui-kit';
 
 import CaseParticipation from './caseparticipation/CaseParticipation';
+import LastContacted from './lastcontacted/LastContacted';
+import { RoleConstants } from './constants';
+import { PERSON_NEIGHBOR_MAP, PROFILE } from './reducers/constants';
 
+import { AppTypes, PropertyTypes } from '../../../core/edm/constants';
+import { getPropertyValue } from '../../../utils/data';
+import { useSelector } from '../../app/AppProvider';
+
+const { PEACEMAKER } = RoleConstants;
+const { ROLE } = AppTypes;
+const { TYPE } = PropertyTypes;
 const { media } = StyleUtils;
+
 const BodyWrapper = styled.div`
   padding: 0 16px;
 
@@ -19,10 +31,15 @@ const BodyWrapper = styled.div`
   `}
 `;
 
-const ProfileBody = () => (
-  <BodyWrapper>
-    <CaseParticipation />
-  </BodyWrapper>
-);
+const ProfileBody = () => {
+  const personRoles :List = useSelector((store) => store.getIn([PROFILE, PERSON_NEIGHBOR_MAP, ROLE], List()));
+  const personIsPeacemaker = personRoles.includes((role :Map) => getPropertyValue(role, TYPE) === PEACEMAKER);
+  return (
+    <BodyWrapper>
+      <CaseParticipation />
+      {personIsPeacemaker && <LastContacted />}
+    </BodyWrapper>
+  );
+};
 
 export default ProfileBody;
