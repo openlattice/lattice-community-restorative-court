1c94398aef8bd678d8883aeeb884f791fbb2e45a
Giulia Campana <giuliaecampana@gmail.com>
2020-10-28 00:15:00 -0700

    new redux constants for profile

--- src/containers/profile/src/reducers/constants/index.js
+++ src/containers/profile/src/reducers/constants/index.js
@@ -1,5 +1,6 @@
 // @flow
 
+const FORM_NEIGHBOR_MAP :string = 'formNeighborMap';
 const PEOPLE_IN_CASE_BY_ROLE_EKID_MAP :string = 'peopleInCaseByRoleEKIDMap';
 const PERSON :string = 'person';
 const PERSON_CASES :string = 'personCases';
@@ -7,8 +8,10 @@ const PERSON_CASE_NEIGHBOR_MAP :string = 'personCaseNeighborMap';
 const PERSON_NEIGHBOR_MAP :string = 'personNeighborMap';
 const PERSON_ROLE_BY_CASE_EKID :string = 'personRoleByCaseEKID';
 const PROFILE :string = 'profile';
+const STAFF_MEMBER_BY_STATUS_EKID :string = 'staffMemberByStatusEKID';
 
 export {
+  FORM_NEIGHBOR_MAP,
   PEOPLE_IN_CASE_BY_ROLE_EKID_MAP,
   PERSON,
   PERSON_CASES,
@@ -16,4 +19,5 @@ export {
   PERSON_NEIGHBOR_MAP,
   PERSON_ROLE_BY_CASE_EKID,
   PROFILE,
+  STAFF_MEMBER_BY_STATUS_EKID,
 };
