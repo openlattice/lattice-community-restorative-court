fa245d035b8836db954be3f4f78837e2be8b3c43
Giulia Campana <giuliaecampana@gmail.com>
2020-10-28 00:14:08 -0700

    getFormNeighbors action

--- src/containers/profile/src/actions/index.js
+++ src/containers/profile/src/actions/index.js
@@ -3,6 +3,9 @@
 import { newRequestSequence } from 'redux-reqseq';
 import type { RequestSequence } from 'redux-reqseq';
 
+const GET_FORM_NEIGHBORS :'GET_FORM_NEIGHBORS' = 'GET_FORM_NEIGHBORS';
+const getFormNeighbors :RequestSequence = newRequestSequence(GET_FORM_NEIGHBORS);
+
 const GET_PERSON :'GET_PERSON' = 'GET_PERSON';
 const getPerson :RequestSequence = newRequestSequence(GET_PERSON);
 
@@ -16,10 +19,12 @@ const LOAD_PROFILE :'LOAD_PROFILE' = 'LOAD_PROFILE';
 const loadProfile :RequestSequence = newRequestSequence(LOAD_PROFILE);
 
 export {
+  GET_FORM_NEIGHBORS,
   GET_PERSON,
   GET_PERSON_CASE_NEIGHBORS,
   GET_PERSON_NEIGHBORS,
   LOAD_PROFILE,
+  getFormNeighbors,
   getPerson,
   getPersonCaseNeighbors,
   getPersonNeighbors,
