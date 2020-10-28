6a160b7c94a9a0307e0ffa889af5cdb864a8d1a5
Giulia Campana <giuliaecampana@gmail.com>
2020-10-28 00:11:08 -0700

    add new app types

--- src/core/edm/constants/AppTypes.js
+++ src/core/edm/constants/AppTypes.js
@@ -6,10 +6,13 @@ const { FQN } = Models;
 
 const APP_TYPE_FQNS = {
   CASE: FQN.of('app.case'),
+  FORM: FQN.of('app.form'),
   HAS: FQN.of('app.has'),
   PEOPLE: FQN.of('app.people'),
+  REFERRAL_REQUEST: FQN.of('app.referralrequest'),
   ROLE: FQN.of('app.role'),
-  STATUS: FQN.of('app.enrollmentstatus'),
+  STAFF: FQN.of('app.staff'),
+  STATUS: FQN.of('app.status'),
 };
 
 export default APP_TYPE_FQNS;
