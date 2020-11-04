c7e8c3de547aa51cd08d22e0283e35f9159a2574
Giulia Campana <giuliaecampana@gmail.com>
2020-11-04 02:35:22 -0800

    add new property types

--- src/core/edm/constants/PropertyTypes.js
+++ src/core/edm/constants/PropertyTypes.js
@@ -8,14 +8,18 @@ const { FQN } = Models;
 
 const PROPERTY_TYPE_FQNS = {
   DATETIME_ADMINISTERED: FQN.of('ol.datetimeadministered'),
+  DATETIME_COMPLETED: FQN.of('date.completeddatetime'),
   DATETIME_START: FQN.of('ol.datetimestart'),
   DESCRIPTION: FQN.of('ol.description'),
   DOB: FQN.of('nc.PersonBirthDate'),
   EFFECTIVE_DATE: FQN.of('ol.effectivedate'),
+  ETHNICITY: FQN.of('nc.PersonEthnicity'),
   GIVEN_NAME: FQN.of('nc.PersonGivenName'),
+  MIDDLE_NAME: FQN.of('nc.PersonMiddleName'),
   NAME: FQN.of('ol.name'),
   OL_DATE_TIME: FQN.of('ol.datetime'),
   OL_ID: FQN.of('ol.id'),
+  RACE: FQN.of('nc.PersonRace'),
   SOURCE: FQN.of('ol.source'),
   STATUS: FQN.of('ol.status'),
   SURNAME: FQN.of('nc.PersonSurName'),
