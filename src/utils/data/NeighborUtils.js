2f90173bc19cea9e9cf12abf4acbfecf49888a22
Giulia Campana <giuliaecampana@gmail.com>
2020-11-04 00:49:44 -0800

    Fix getNeighborDetails bug

--- src/utils/data/NeighborUtils.js
+++ src/utils/data/NeighborUtils.js
@@ -1,7 +1,7 @@
 // @flow
-import { Map, getIn } from 'immutable';
+import { Map, get, getIn } from 'immutable';
 
-const getNeighborDetails = (neighbor :Map | Object) :Map | Object => getIn(neighbor, 'neighborDetails');
+const getNeighborDetails = (neighbor :Map | Object) :Map | Object => get(neighbor, 'neighborDetails');
 
 const getNeighborESID = (neighbor :Map | Object) :UUID => getIn(neighbor, ['neighborEntitySet', 'id']);
 
