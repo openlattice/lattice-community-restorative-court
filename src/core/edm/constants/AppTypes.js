a52bc95fbe9078ffb5d283abe08ee626b49b4f83
Giulia Campana <giuliaecampana@gmail.com>
2020-11-16 10:06:01 -0800

    new app type

--- src/core/edm/constants/AppTypes.js
+++ src/core/edm/constants/AppTypes.js
@@ -10,6 +10,7 @@ const APP_TYPE_FQNS = {
   FORM: FQN.of('app.form'),
   HAS: FQN.of('app.has'),
   PEOPLE: FQN.of('app.people'),
+  RECORDED_BY: FQN.of('app.recordedby'),
   REFERRAL_REQUEST: FQN.of('app.referralrequest'),
   ROLE: FQN.of('app.role'),
   STAFF: FQN.of('app.staff'),
