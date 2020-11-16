167b250cf3d597d8468d25e583e5de371f2737e2
Giulia Campana <giuliaecampana@gmail.com>
2020-11-16 07:16:16 -0800

    use consts in RoleTag style util functions

--- src/components/roles/RoleTag.js
+++ src/components/roles/RoleTag.js
@@ -2,19 +2,22 @@
 import styled from 'styled-components';
 import { Colors, StyleUtils, Tag } from 'lattice-ui-kit';
 
+import { RoleConstants } from '../../containers/profile/src/constants';
+
 const { getStyleVariation } = StyleUtils;
 const { BLUE, GREEN, RED } = Colors;
+const { PEACEMAKER, RESPONDENT, VICTIM } = RoleConstants;
 
 export const getBackgroundColor = getStyleVariation('roleName', {
-  peacemaker: BLUE.B00,
-  respondent: RED.R00,
-  victim: GREEN.G00,
+  [PEACEMAKER]: BLUE.B00,
+  [RESPONDENT]: RED.R00,
+  [VICTIM]: GREEN.G00,
 }, RED.R00);
 
 export const getFontColor = getStyleVariation('roleName', {
-  peacemaker: BLUE.B400,
-  respondent: RED.R400,
-  victim: GREEN.G400,
+  [PEACEMAKER]: BLUE.B400,
+  [RESPONDENT]: RED.R400,
+  [VICTIM]: GREEN.G400,
 }, RED.R400);
 
 export const RoleTag = styled(Tag)`
