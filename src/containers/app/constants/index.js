58c1291579267c2e85f4c48044d21b23c5cf6492
Giulia Campana <giuliaecampana@gmail.com>
2020-11-16 10:05:00 -0800

    app redux consts

--- src/containers/app/constants/index.js
+++ src/containers/app/constants/index.js
@@ -5,6 +5,7 @@ const APP_NAME = 'Community Restorative Court';
 const APP :string = 'app';
 const APP_CONFIG :string = 'appConfig';
 const ENTITY_SET_ID :string = 'entitySetId';
+const ENTITY_SET_IDS = 'entitySetIds';
 const FQNS_BY_ESID :string = 'fqnsByESID';
 const MATCH :string = 'match';
 const ROOT :string = 'root';
@@ -22,10 +23,12 @@ const NEIGHBOR_DIRECTIONS = {
 };
 
 export {
+  APP,
   APP_CONFIG,
   APP_NAME,
   APP_PATHS,
   ENTITY_SET_ID,
+  ENTITY_SET_IDS,
   FQNS_BY_ESID,
   MATCH,
   NEIGHBOR_DIRECTIONS,
