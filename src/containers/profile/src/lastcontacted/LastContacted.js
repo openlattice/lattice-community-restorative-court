86e9850f684397cf563e2418c866c2656ca98517
Giulia Campana <giuliaecampana@gmail.com>
2020-11-16 16:51:45 -0800

    fix LastContacted bugs

--- src/containers/profile/src/lastcontacted/LastContacted.js
+++ src/containers/profile/src/lastcontacted/LastContacted.js
@@ -6,42 +6,50 @@ import { faPlus } from '@fortawesome/pro-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { List, Map } from 'immutable';
 import { Colors, IconButton, StyleUtils } from 'lattice-ui-kit';
-import { DateTimeUtils } from 'lattice-utils';
+import { DataUtils, DateTimeUtils } from 'lattice-utils';
 
 import { AppTypes, PropertyTypes } from '../../../../core/edm/constants';
-import { getPropertyValuesLU } from '../../../../utils/data';
 import { useSelector } from '../../../app/AppProvider';
 import { Header } from '../../typography';
+import { ContactConstants } from '../constants';
 import { PERSON_NEIGHBOR_MAP, PROFILE } from '../reducers/constants';
 
 const { CONTACT_ACTIVITY } = AppTypes;
 const { CONTACT_DATETIME, OUTCOME } = PropertyTypes;
 const { GREEN, NEUTRAL, RED } = Colors;
+const { FAILURE, SUCCESS } = ContactConstants;
+const { getPropertyValue } = DataUtils;
 const { formatAsDate } = DateTimeUtils;
 const { getStyleVariation } = StyleUtils;
 
 const getBackgroundColor = getStyleVariation('outcome', {
-  failure: RED.R00,
-  success: GREEN.G00,
+  [FAILURE]: RED.R00,
+  [SUCCESS]: GREEN.G00,
 }, RED.R00);
 
 const getFontColor = getStyleVariation('outcome', {
-  failure: RED.R400,
-  success: GREEN.G400,
+  [FAILURE]: RED.R400,
+  [SUCCESS]: GREEN.G400,
 }, RED.R400);
 
 const ContactTag = styled.div`
   background-color: ${getBackgroundColor};
   border-radius: 5px;
   color: ${getFontColor};
-  display: grid;
-  grid-gap: 24px 16px;
+  font-size: 16px;
+  font-weight: 600;
   padding: 8px;
 `;
 
+const SectionWrapper = styled.div`
+  margin-top: 36px;
+`;
+
 const ContactGrid = styled.div`
-  align-items: stretch;
-  grid-template-columns: repeat(auto-fit, minmax(98px, 1fr));
+  display: grid;
+  grid-gap: 24px 16px;
+  grid-template-columns: repeat(auto-fit, 98px);
+  margin-bottom: 36px;
 `;
 
 const HeaderRow = styled.div`
@@ -56,7 +64,7 @@ const LastContacted = () => {
     .getIn([PROFILE, PERSON_NEIGHBOR_MAP, CONTACT_ACTIVITY], List()));
   const sortedContactActivity :List = contactActivity.sortBy((contactMade :Map) => contactMade.get(CONTACT_DATETIME));
   return (
-    <div>
+    <SectionWrapper>
       <HeaderRow>
         <Header>Last Contacted</Header>
         <IconButton onClick={() => setModalVisibility(true)}>
@@ -66,18 +74,16 @@ const LastContacted = () => {
       <ContactGrid>
         {
           sortedContactActivity.map((contactMade :Map) => {
-            const { [CONTACT_DATETIME]: datetime, [OUTCOME]: outcome } = getPropertyValuesLU(
-              contactMade,
-              [CONTACT_DATETIME, OUTCOME]
-            );
+            const datetime = getPropertyValue(contactMade, [CONTACT_DATETIME, 0]);
             const date = formatAsDate(datetime);
+            const outcome = getPropertyValue(contactMade, [OUTCOME, 0]);
             return (
               <ContactTag outcome={outcome}>{date}</ContactTag>
             );
           })
         }
       </ContactGrid>
-    </div>
+    </SectionWrapper>
   );
 };
 
