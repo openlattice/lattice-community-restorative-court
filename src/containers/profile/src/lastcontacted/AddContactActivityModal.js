baebff793e31962f8a8a3b959311e29631f8bc7c
Giulia Campana <giuliaecampana@gmail.com>
2020-11-16 17:34:54 -0800

    update AddContactActivityModal

--- src/containers/profile/src/lastcontacted/AddContactActivityModal.js
+++ src/containers/profile/src/lastcontacted/AddContactActivityModal.js
@@ -1,8 +1,31 @@
 // @flow
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { Form } from 'lattice-fabricate';
-import { Modal } from 'lattice-ui-kit';
+import { Map, getIn, setIn } from 'immutable';
+import { DataProcessingUtils, Form } from 'lattice-fabricate';
+import { Modal, ModalFooter } from 'lattice-ui-kit';
+import { DataUtils, ReduxUtils } from 'lattice-utils';
+import { DateTime } from 'luxon';
+
+import { AppTypes, PropertyTypes } from '../../../../core/edm/constants';
+import { resetRequestState } from '../../../../core/redux/actions';
+import { EDM, PROPERTY_TYPE_IDS, REQUEST_STATE } from '../../../../core/redux/constants';
+import { useDispatch, useSelector } from '../../../app/AppProvider';
+import { APP, ENTITY_SET_IDS } from '../../../app/constants';
+import { ADD_CONTACT_ACTIVITY, addContactActivity } from '../actions';
+import { PERSON, PROFILE } from '../reducers/constants';
+import { schema, uiSchema } from '../schemas/AddContactActivitySchemas';
+
+const { CONTACT_ACTIVITY, PEOPLE, SENT_TO } = AppTypes;
+const { CONTACT_DATETIME } = PropertyTypes;
+const { isPending, isSuccess } = ReduxUtils;
+const {
+  getEntityAddressKey,
+  getPageSectionKey,
+  processAssociationEntityData,
+  processEntityData,
+} = DataProcessingUtils;
+const { getEntityKeyId } = DataUtils;
 
 type Props = {
   isVisible :boolean;
@@ -10,17 +33,71 @@ type Props = {
 };
 
 const AddContactActivityModal = ({ isVisible, onClose } :Props) => {
-  const onSubmit = ({ formData }) => {
 
+  const [formData, setFormData] = useState({});
+
+  const onChange = ({ formData: updatedFormData } :Object) => {
+    setFormData(updatedFormData);
   };
+
+  const entitySetIds :Map = useSelector((store) => store.getIn([APP, ENTITY_SET_IDS]));
+  const propertyTypeIds :Map = useSelector((store) => store.getIn([EDM, PROPERTY_TYPE_IDS]));
+  const person :Map = useSelector((store) => store.getIn([PROFILE, PERSON]));
+  const personEKID :?UUID = getEntityKeyId(person);
+  const dispatch = useDispatch();
+
+  const onSubmit = () => {
+    const contactDatetimePath = [getPageSectionKey(1, 1), getEntityAddressKey(0, CONTACT_ACTIVITY, CONTACT_DATETIME)];
+    const date = getIn(formData, contactDatetimePath);
+    const now = DateTime.local();
+    const updatedFormData = setIn(
+      formData,
+      contactDatetimePath,
+      DateTime.fromSQL(date.concat(' ', now.toISOTime())).toISO()
+    );
+
+    const entityData = processEntityData(updatedFormData, entitySetIds, propertyTypeIds);
+    const associations = [
+      [SENT_TO, 0, CONTACT_ACTIVITY, personEKID, PEOPLE, {}]
+    ];
+    const associationEntityData = processAssociationEntityData(associations, entitySetIds, propertyTypeIds);
+
+    dispatch(addContactActivity({ associationEntityData, entityData }));
+  };
+
+  const submitRequestState = useSelector((store :Map) => store.getIn([PROFILE, ADD_CONTACT_ACTIVITY, REQUEST_STATE]));
+
+  useEffect(() => {
+    if (isSuccess(submitRequestState)) {
+      dispatch(resetRequestState([PROFILE, ADD_CONTACT_ACTIVITY]));
+      onClose();
+    }
+  }, [dispatch, onClose, submitRequestState]);
+
+  const withFooter = (
+    <ModalFooter
+        isPendingPrimary={isPending(submitRequestState)}
+        onClickPrimary={onSubmit}
+        onClickSecondary={onClose}
+        textPrimary="Submit"
+        textSecondary="Discard" />
+  );
+
   return (
     <Modal
         isVisible={isVisible}
         onClose={onClose}
-        viewportScrolling>
+        textTitle="Add Contact Activity"
+        viewportScrolling
+        withFooter={withFooter}>
       <Form
+          formData={formData}
           hideSubmit
-          onSubmit={onSubmit} />
+          noPadding
+          onChange={onChange}
+          onSubmit={onSubmit}
+          schema={schema}
+          uiSchema={uiSchema} />
     </Modal>
   );
 };
