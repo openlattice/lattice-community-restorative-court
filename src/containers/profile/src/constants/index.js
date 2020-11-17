8c377aceb6f8aff8ef5a447c7f86e9d25b79b536
Giulia Campana <giuliaecampana@gmail.com>
2020-11-16 16:51:34 -0800

    put ContactConstants in index

--- src/containers/profile/src/constants/index.js
+++ src/containers/profile/src/constants/index.js
@@ -1,12 +1,14 @@
 // @flow
 
 import * as CaseStatusConstants from './CaseStatusConstants';
+import * as ContactConstants from './ContactConstants';
 import * as RoleConstants from './RoleConstants';
 
 const EMPTY_VALUE = '---';
 
 export {
   CaseStatusConstants,
+  ContactConstants,
   RoleConstants,
   EMPTY_VALUE
 };
