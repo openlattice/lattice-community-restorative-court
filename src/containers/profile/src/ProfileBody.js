8b490478ab7648a9030c1aaaf7f17e6267b9b55d
Giulia Campana <giuliaecampana@gmail.com>
2020-11-18 15:53:16 -0800

    remove responsive style for phone ProfileBody

--- src/containers/profile/src/ProfileBody.js
+++ src/containers/profile/src/ProfileBody.js
@@ -2,21 +2,15 @@
 import React from 'react';
 
 import styled from 'styled-components';
-import { StyleUtils } from 'lattice-ui-kit';
 
 import CaseParticipation from './caseparticipation/CaseParticipation';
 
-const { media } = StyleUtils;
 const BodyWrapper = styled.div`
   padding: 0 16px;
 
   .MuiTabPanel-root {
     padding: 16px 0;
   }
-
-  ${media.phone`
-    padding: 0;
-  `}
 `;
 
 const ProfileBody = () => (
