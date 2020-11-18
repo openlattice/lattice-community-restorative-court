9aa840725e9afba961956f79a9c005fd11ba3467
Giulia Campana <giuliaecampana@gmail.com>
2020-11-18 12:40:47 -0800

    form typography

--- src/containers/profile/typography/index.js
+++ src/containers/profile/typography/index.js
@@ -10,7 +10,25 @@ const Header = styled.h2`
   margin: 0 0 24px;
 `;
 
+const FormHeader = styled.h1`
+  color: ${NEUTRAL.N900};
+  font-size: 28px;
+  font-weight: 600;
+  padding-left: 30px;
+  padding-right: 30px;
+`;
+
+const FormDescription = styled.div`
+  color: ${NEUTRAL.N500};
+  display: flex;
+  font-size: 16px;
+  padding-left: 30px;
+  padding-right: 30px;
+`;
+
 /* eslint-disable import/prefer-default-export */
 export {
+  FormDescription,
+  FormHeader,
   Header,
 };
