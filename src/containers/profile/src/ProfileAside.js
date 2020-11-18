6b9477335a21f65528a14f471a33f5cbf5e05e4d
Giulia Campana <giuliaecampana@gmail.com>
2020-11-18 12:53:12 -0800

    add ChooseFormTypeModal to profile

--- src/containers/profile/src/ProfileAside.js
+++ src/containers/profile/src/ProfileAside.js
@@ -1,23 +1,35 @@
 // @flow
-import React from 'react';
+import React, { useState } from 'react';
 
 import styled from 'styled-components';
+import { Button } from 'lattice-ui-kit';
 
 import ProfileCard from './ProfileCard';
 import { PERSON, PROFILE } from './reducers/constants';
 
+import ChooseFormTypeModal from '../../../components/forms/ChooseFormTypeModal';
 import { useSelector } from '../../app/AppProvider';
 
 const Centered = styled.div`
   align-items: center;
 `;
 
-const ProfileAside = () => {
-  const person = useSelector((store) => store.getIn([PROFILE, PERSON]));
+const ButtonWrapper = styled.div`
+  display: flex;
+  align-content: stretch;
+  margin-top: 16px;
+`;
 
+const ProfileAside = () => {
+  const [formChoiceModalIsVisible, setFormChoiceModalVisibility] = useState(false);
+  const person = useSelector((store) => store.getIn([PROFILE, PERSON]));
   return (
     <Centered>
       <ProfileCard person={person} />
+      <ButtonWrapper>
+        <Button color="primary" onClick={() => setFormChoiceModalVisibility(true)}>Add Form</Button>
+      </ButtonWrapper>
+      <ChooseFormTypeModal isVisible={formChoiceModalIsVisible} onClose={() => setFormChoiceModalVisibility(false)} />
     </Centered>
   );
 };
