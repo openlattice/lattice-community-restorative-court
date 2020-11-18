302dd7f8cc5e0e6b27603db09ab621e9f8f02d2b
Giulia Campana <giuliaecampana@gmail.com>
2020-11-18 12:53:18 -0800

    new route constants

--- src/core/router/Routes.js
+++ src/core/router/Routes.js
@@ -4,6 +4,11 @@
 
 const ROOT :'/' = '/';
 
+const PERSON_ID :string = ':personId';
+const PEACEMAKER_INFORMATION :string = 'peacemakerinformation';
+
 export {
+  PERSON_ID,
+  PEACEMAKER_INFORMATION,
   ROOT,
 };
