f3f674b3f6624083b27656d381966128f0253f58
Giulia Campana <giuliaecampana@gmail.com>
2020-11-19 04:07:38 -0800

    make the PeacemakerInformationForm editable

--- src/containers/peacemaker/PeacemakerInformationForm.js
+++ src/containers/peacemaker/PeacemakerInformationForm.js
@@ -1,47 +1,102 @@
 // @flow
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Map } from 'immutable';
+import { List, Map } from 'immutable';
 import { DataProcessingUtils, Form } from 'lattice-fabricate';
-import { ReduxUtils } from 'lattice-utils';
+import { DataUtils, LangUtils, ReduxUtils } from 'lattice-utils';
+import type { UUID } from 'lattice';
 
-import { ADD_PEACEMAKER_INFORMATION, addPeacemakerInformation } from './actions';
+import { ADD_PEACEMAKER_INFORMATION, addPeacemakerInformation, editPeacemakerInformation } from './actions';
 import { PEACEMAKER } from './reducers/constants';
 import { schema, uiSchema } from './schemas/PeacemakerInformationSchemas';
+import { prepopulateForm } from './utils';
 
-import { AppTypes } from '../../core/edm/constants';
-import { EDM, PROPERTY_TYPE_IDS, REQUEST_STATE } from '../../core/redux/constants';
+import { AppTypes, PropertyTypes } from '../../core/edm/constants';
+import {
+  APP,
+  APP_REDUX_CONSTANTS,
+  EDM,
+  PROPERTY_TYPE_IDS,
+  ProfileReduxConstants,
+  REQUEST_STATE,
+} from '../../core/redux/constants';
 import { useDispatch, useSelector } from '../app/AppProvider';
-import { APP, ENTITY_SET_IDS } from '../app/constants';
+import { FormConstants } from '../profile/src/constants';
 
 const {
+  COMMUNICATION,
   FORM,
   HAS,
   PEOPLE,
   PERSON_DETAILS,
   SCREENED_WITH,
 } = AppTypes;
+const { NAME } = PropertyTypes;
+const { PERSON_NEIGHBOR_MAP, PROFILE } = ProfileReduxConstants;
+const { PEACEMAKER_INFORMATION_FORM } = FormConstants;
 const { processAssociationEntityData, processEntityData } = DataProcessingUtils;
 const { isPending } = ReduxUtils;
+const { getEntityKeyId, getPropertyValue } = DataUtils;
+const { isDefined } = LangUtils;
 
 type Props = {
   personId :UUID;
 };
 
 const PeacemakerInformationForm = ({ personId } :Props) => {
-
-  const entitySetIds :Map = useSelector((store) => store.getIn([APP, ENTITY_SET_IDS]));
-  const propertyTypeIds :Map = useSelector((store) => store.getIn([EDM, PROPERTY_TYPE_IDS]));
   const dispatch = useDispatch();
 
-  const onSubmit = ({ formData }) => {
+  const personNeighborMap :Map = useSelector((store) => store.getIn([PROFILE, PERSON_NEIGHBOR_MAP], Map()));
+
+  const forms :List = personNeighborMap.get(FORM, List());
+  const personInformationForm :?Map = forms
+    .find((form :Map) => getPropertyValue(form, [NAME, 0]) === PEACEMAKER_INFORMATION_FORM);
+  const formEKID :?UUID = isDefined(personInformationForm) ? getEntityKeyId(personInformationForm) : '';
+
+  let prepopulatedFormData = {};
+  if (isDefined(personInformationForm)) {
+    prepopulatedFormData = prepopulateForm(personNeighborMap);
+  }
+  const [formData, setFormData] = useState(prepopulatedFormData);
+
+  const onChange = ({ formData: updatedFormData } :Object) => {
+    setFormData(updatedFormData);
+  };
+
+  const entitySetIds :Map = useSelector((store) => store.getIn([APP, APP_REDUX_CONSTANTS.ENTITY_SET_IDS]));
+  const propertyTypeIds :Map = useSelector((store) => store.getIn([EDM, PROPERTY_TYPE_IDS]));
+
+  const onSubmit = () => {
     const entityData = processEntityData(formData, entitySetIds, propertyTypeIds);
     const associations = [
       [SCREENED_WITH, personId, PEOPLE, 0, FORM, {}],
       [HAS, personId, PEOPLE, 0, PERSON_DETAILS, {}],
+      [HAS, personId, PEOPLE, 0, COMMUNICATION, {}],
     ];
     const associationEntityData = processAssociationEntityData(associations, entitySetIds, propertyTypeIds);
     dispatch(addPeacemakerInformation({ associationEntityData, entityData }));
+  };
+
+  const handleEdit = (params) => {
+    dispatch(editPeacemakerInformation({ ...params }));
+  };
+
+  const communication :List = useSelector((store) => store
+    .getIn([PROFILE, PERSON_NEIGHBOR_MAP, COMMUNICATION], List()));
+  const personDetails :List = useSelector((store) => store
+    .getIn([PROFILE, PERSON_NEIGHBOR_MAP, PERSON_DETAILS], List()));
+
+  const entityIndexToIdMap :Map = Map.withMutations((mutator :Map) => {
+    mutator.set(COMMUNICATION, List([getEntityKeyId(communication.get(0))]));
+    mutator.set(FORM, List([formEKID]));
+    mutator.set(PERSON_DETAILS, List([getEntityKeyId(personDetails.get(0))]));
+  });
+
+  const formContext = {
+    editAction: handleEdit,
+    entityIndexToIdMap,
+    entitySetIds,
+    propertyTypeIds,
   };
 
   const submitRequestState = useSelector((store :Map) => store
@@ -49,7 +104,11 @@ const PeacemakerInformationForm = ({ personId } :Props) => {
 
   return (
     <Form
+        disabled={isDefined(personInformationForm) && !personInformationForm.isEmpty()}
+        formContext={formContext}
+        formData={formData}
         isSubmitting={isPending(submitRequestState)}
+        onChange={onChange}
         onSubmit={onSubmit}
         schema={schema}
         uiSchema={uiSchema} />
