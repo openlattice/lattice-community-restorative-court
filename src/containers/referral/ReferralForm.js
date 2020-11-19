f90e787d2b958636c908f0ef2af1b4ea3e5b1037
Giulia Campana <giuliaecampana@gmail.com>
2020-11-19 04:40:20 -0800

    update ReferralForm

--- src/containers/referral/ReferralForm.js
+++ src/containers/referral/ReferralForm.js
@@ -1,15 +1,22 @@
 // @flow
 import React from 'react';
 
-import styled from 'styled-components';
-import { getIn } from 'immutable';
+import { Map, getIn, setIn } from 'immutable';
 import { DataProcessingUtils, Form } from 'lattice-fabricate';
+import { Typography } from 'lattice-ui-kit';
 import { DateTime } from 'luxon';
+import type { UUID } from 'lattice';
 
 import { schema, uiSchema } from './schemas/ReferralFormSchemas';
 
 import { AppTypes, PropertyTypes } from '../../core/edm/constants';
-import { FormDescription, FormHeader } from '../profile/typography';
+import {
+  APP,
+  APP_REDUX_CONSTANTS,
+  EDM,
+  PROPERTY_TYPE_IDS,
+} from '../../core/redux/constants';
+import { useDispatch, useSelector } from '../app/AppProvider';
 
 const {
   getEntityAddressKey,
@@ -17,22 +24,67 @@ const {
   processAssociationEntityData,
   processEntityData,
 } = DataProcessingUtils;
-const { REFERRAL_REQUEST } = AppTypes;
+const {
+  APPEARS_IN,
+  CRC_CASE,
+  DA_CASE,
+  FORM,
+  OFFENSE,
+  OFFICERS,
+  PEOPLE,
+  REFERRAL_REQUEST,
+  REGISTERED_FOR,
+  RELATED_TO,
+  RESULTS_IN,
+  SCREENED_WITH,
+  SENT_FROM,
+  STATUS,
+  SUBJECT_OF,
+} = AppTypes;
 const { DATETIME_COMPLETED } = PropertyTypes;
 
-const ReferralForm = () => {
+type Props = {
+  personId :UUID;
+};
+
+const ReferralForm = ({ personId } :Props) => {
+
+  const dispatch = useDispatch();
+
+  const entitySetIds :Map = useSelector((store) => store.getIn([APP, APP_REDUX_CONSTANTS.ENTITY_SET_IDS]));
+  const propertyTypeIds :Map = useSelector((store) => store.getIn([EDM, PROPERTY_TYPE_IDS]));
+
   const onSubmit = ({ formData } :Object) => {
+
     const dateOfReferralPath = [getPageSectionKey(1, 1), getEntityAddressKey(0, REFERRAL_REQUEST, DATETIME_COMPLETED)];
     const dateOfReferral = getIn(formData, dateOfReferralPath);
     const now = DateTime.local();
-    const updatedFormData = setIn(formData, dateOfReferralPath, DateTime.fromSQL(dateOfReferral.concat(' ', now.toISOTime()))).toISO();
+    const updatedFormData = setIn(
+      formData,
+      dateOfReferralPath,
+      DateTime.fromSQL(dateOfReferral.concat(' ', now.toISOTime()))
+    ).toISO();
+
+    const entityData = processEntityData(updatedFormData, entitySetIds, propertyTypeIds);
+    const associations = [
+      [APPEARS_IN, 0, DA_CASE, 0, REFERRAL_REQUEST, {}],
+      [RESULTS_IN, 0, REFERRAL_REQUEST, 0, CRC_CASE, {}],
+      [SUBJECT_OF, personId, PEOPLE, 0, REFERRAL_REQUEST, {}],
+      [APPEARS_IN, 0, OFFENSE, 0, REFERRAL_REQUEST, {}],
+      [REGISTERED_FOR, 0, STATUS, 0, REFERRAL_REQUEST, {}],
+      [SENT_FROM, 0, REFERRAL_REQUEST, 0, OFFICERS, {}],
+      [SCREENED_WITH, 0, PEOPLE, 0, FORM, {}],
+      [RELATED_TO, 0, FORM, 0, REFERRAL_REQUEST, {}],
+    ];
+    const associationEntityData = processAssociationEntityData(associations, entitySetIds, propertyTypeIds);
+    // dispatch(submitReferralForm({ associationEntityData, entityData }));
   };
   return (
     <>
-      <FormHeader>Referral Information</FormHeader>
-      <FormDescription>
+      <Typography variant="h3">Referral Information</Typography>
+      <Typography>
         Enter the respondent and victim information below to add a new referral for this person.
-      </FormDescription>
+      </Typography>
       <Form
           onSubmit={onSubmit}
           schema={schema}
