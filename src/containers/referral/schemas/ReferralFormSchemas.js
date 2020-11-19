2b10f465e07c1ff92020ddcd73387afe36c9db67
Giulia Campana <giuliaecampana@gmail.com>
2020-11-19 04:42:57 -0800

    fix schemas

--- src/containers/referral/schemas/ReferralFormSchemas.js
+++ src/containers/referral/schemas/ReferralFormSchemas.js
@@ -83,53 +83,50 @@ const schema = {
       type: 'array',
       title: 'Victim Information',
       items: {
-        [getPageSectionKey(1, 3)]: {
-          type: 'object',
-          title: '',
-          properties: {
-            [getEntityAddressKey(0, PEOPLE, SURNAME)]: {
-              type: 'string',
-              title: 'Last Name',
-            },
-            [getEntityAddressKey(0, PEOPLE, GIVEN_NAME)]: {
-              type: 'string',
-              title: 'First Name',
-            },
-            [getEntityAddressKey(0, PEOPLE, MIDDLE_NAME)]: {
-              type: 'string',
-              title: 'Middle Name',
-            },
-            [getEntityAddressKey(0, PEOPLE, DOB)]: {
-              type: 'string',
-              title: 'Date of Birth',
-              format: 'date'
-            },
-            [getEntityAddressKey(0, PERSON_DETAILS, GENDER)]: {
-              type: 'string',
-              title: 'Gender',
-              enum: GENDERS,
-            },
-            [getEntityAddressKey(0, PERSON_DETAILS, GENDER)]: {
-              type: 'string',
-              title: 'Preferred Pronouns',
-            },
-            [getEntityAddressKey(0, PEOPLE, RACE)]: {
-              type: 'string',
-              title: 'Race',
-              enum: RACES,
-            },
-            [getEntityAddressKey(0, PEOPLE, ETHNICITY)]: {
-              type: 'string',
-              title: 'Ethnicity',
-              enum: ETHNICITIES,
-            },
-          }
+        type: 'object',
+        properties: {
+          [getEntityAddressKey(0, PEOPLE, SURNAME)]: {
+            type: 'string',
+            title: 'Last Name',
+          },
+          [getEntityAddressKey(0, PEOPLE, GIVEN_NAME)]: {
+            type: 'string',
+            title: 'First Name',
+          },
+          [getEntityAddressKey(0, PEOPLE, MIDDLE_NAME)]: {
+            type: 'string',
+            title: 'Middle Name',
+          },
+          [getEntityAddressKey(0, PEOPLE, DOB)]: {
+            type: 'string',
+            title: 'Date of Birth',
+            format: 'date'
+          },
+          [getEntityAddressKey(0, PERSON_DETAILS, GENDER)]: {
+            type: 'string',
+            title: 'Gender',
+            enum: GENDERS,
+          },
+          [getEntityAddressKey(0, PERSON_DETAILS, GENDER)]: {
+            type: 'string',
+            title: 'Preferred Pronouns',
+          },
+          [getEntityAddressKey(0, PEOPLE, RACE)]: {
+            type: 'string',
+            title: 'Race',
+            enum: RACES,
+          },
+          [getEntityAddressKey(0, PEOPLE, ETHNICITY)]: {
+            type: 'string',
+            title: 'Ethnicity',
+            enum: ETHNICITIES,
+          },
         }
       }
     },
     [getPageSectionKey(1, 3)]: {
       type: 'object',
-      title: 'Hidden Form Info',
+      title: 'Form Info',
       properties: {
         [getEntityAddressKey(0, FORM, DATETIME_ADMINISTERED)]: {
           type: 'string',
@@ -176,30 +173,38 @@ const uiSchema = {
   },
   [getPageSectionKey(1, 2)]: {
     classNames: 'column-span-12 grid-container',
-    [getEntityAddressKey(0, PEOPLE, SURNAME)]: {
-      classNames: 'column-span-4'
+    'ui:options': {
+      addButtonText: '+ Add Court Hearing',
+      orderable: false,
+      addActionKey: 'addCourtHearing'
     },
-    [getEntityAddressKey(0, PEOPLE, GIVEN_NAME)]: {
-      classNames: 'column-span-4'
-    },
-    [getEntityAddressKey(0, PEOPLE, MIDDLE_NAME)]: {
-      classNames: 'column-span-4'
-    },
-    [getEntityAddressKey(0, PEOPLE, DOB)]: {
-      classNames: 'column-span-4'
-    },
-    [getEntityAddressKey(0, PERSON_DETAILS, GENDER)]: {
-      classNames: 'column-span-4'
-    },
-    [getEntityAddressKey(0, PERSON_DETAILS, GENDER)]: {
-      classNames: 'column-span-4'
-    },
-    [getEntityAddressKey(0, PEOPLE, RACE)]: {
-      classNames: 'column-span-4'
-    },
-    [getEntityAddressKey(0, PEOPLE, ETHNICITY)]: {
-      classNames: 'column-span-4'
-    },
+    items: {
+      classNames: 'grid-container',
+      [getEntityAddressKey(0, PEOPLE, SURNAME)]: {
+        classNames: 'column-span-4'
+      },
+      [getEntityAddressKey(0, PEOPLE, GIVEN_NAME)]: {
+        classNames: 'column-span-4'
+      },
+      [getEntityAddressKey(0, PEOPLE, MIDDLE_NAME)]: {
+        classNames: 'column-span-4'
+      },
+      [getEntityAddressKey(0, PEOPLE, DOB)]: {
+        classNames: 'column-span-4'
+      },
+      [getEntityAddressKey(0, PERSON_DETAILS, GENDER)]: {
+        classNames: 'column-span-4'
+      },
+      [getEntityAddressKey(0, PERSON_DETAILS, GENDER)]: {
+        classNames: 'column-span-4'
+      },
+      [getEntityAddressKey(0, PEOPLE, RACE)]: {
+        classNames: 'column-span-4'
+      },
+      [getEntityAddressKey(0, PEOPLE, ETHNICITY)]: {
+        classNames: 'column-span-4'
+      },
+    }
   },
   [getPageSectionKey(1, 3)]: {
     classNames: 'column-span-12 grid-container',
