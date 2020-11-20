15638ad8c6919db18ffbaeb3c23ec69128466d9b
Giulia Campana <giulia@openlattice.com>
2020-11-20 05:15:36 -0800

    more referral redux constants

--- src/core/redux/constants/ReferralReduxConstants.js
+++ src/core/redux/constants/ReferralReduxConstants.js
@@ -1,8 +1,12 @@
 // @flow
-const REFERRAL :string = 'referral';
 const CRC_PEOPLE :string = 'crcPeople';
+const REFERRAL :string = 'referral';
+const REFERRAL_REQUEST_NEIGHBOR_MAP :string = 'referralRequestNeighborMap';
+const SELECTED_REFERRAL_FORM :string = 'selectedReferralForm';
 
 export {
   CRC_PEOPLE,
   REFERRAL,
+  REFERRAL_REQUEST_NEIGHBOR_MAP,
+  SELECTED_REFERRAL_FORM,
 };
