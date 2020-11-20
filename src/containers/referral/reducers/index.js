fa362d8def98ed2b1583cc49d6533264e6ca2d82
Giulia Campana <giulia@openlattice.com>
2020-11-20 05:17:30 -0800

    Referral Reducer

--- src/containers/referral/reducers/index.js
+++ src/containers/referral/reducers/index.js
@@ -2,6 +2,8 @@
 import { List, Map, fromJS } from 'immutable';
 
 import getCRCPeopleReducer from './getCRCPeopleReducer';
+import getReferralRequestNeighborsReducer from './getReferralRequestNeighborsReducer';
+import selectReferralFormReducer from './selectReferralFormReducer';
 import submitReferralFormReducer from './submitReferralFormReducer';
 
 import { RESET_REQUEST_STATE } from '../../../core/redux/actions';
@@ -9,19 +11,25 @@ import { RS_INITIAL_STATE, ReferralReduxConstants } from '../../../core/redux/co
 import { resetRequestStateReducer } from '../../../core/redux/reducers';
 import {
   GET_CRC_PEOPLE,
+  GET_REFERRAL_REQUEST_NEIGHBORS,
+  SELECT_REFERRAL_FORM,
   SUBMIT_REFERRAL_FORM,
   getCRCPeople,
+  getReferralRequestNeighbors,
   submitReferralForm,
 } from '../actions';
 
-const { CRC_PEOPLE } = ReferralReduxConstants;
+const { CRC_PEOPLE, REFERRAL_REQUEST_NEIGHBOR_MAP, SELECTED_REFERRAL_FORM } = ReferralReduxConstants;
 
 const INITIAL_STATE :Map = fromJS({
   // actions
   [GET_CRC_PEOPLE]: RS_INITIAL_STATE,
+  [GET_REFERRAL_REQUEST_NEIGHBORS]: RS_INITIAL_STATE,
   [SUBMIT_REFERRAL_FORM]: RS_INITIAL_STATE,
   // data
   [CRC_PEOPLE]: List(),
+  [REFERRAL_REQUEST_NEIGHBOR_MAP]: Map(),
+  [SELECTED_REFERRAL_FORM]: Map(),
 });
 
 export default function profileReducer(state :Map = INITIAL_STATE, action :Object) {
@@ -32,8 +40,15 @@ export default function profileReducer(state :Map = INITIAL_STATE, action :Objec
       return resetRequestStateReducer(state, action);
     }
 
+    case SELECT_REFERRAL_FORM: {
+      return selectReferralFormReducer(state, action);
+    }
+
     case getCRCPeople.case(action.type):
       return getCRCPeopleReducer(state, action);
+
+    case getReferralRequestNeighbors.case(action.type):
+      return getReferralRequestNeighborsReducer(state, action);
 
     case submitReferralForm.case(action.type):
       return submitReferralFormReducer(state, action);
