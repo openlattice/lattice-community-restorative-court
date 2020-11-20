58951218d1014ae7ce196d46cf856d71d99c4fd1
Giulia Campana <giulia@openlattice.com>
2020-11-19 22:53:16 -0800

    updateFormWithDateAsDateTime util

---
+++ src/utils/form/updateFormWithDateAsDateTime.js
@@ -0,0 +1,24 @@
+// @flow
+
+import { Map, getIn, setIn } from 'immutable';
+import { LangUtils } from 'lattice-utils';
+import { DateTime } from 'luxon';
+
+const { isDefined } = LangUtils;
+
+const updateFormWithDateAsDateTime = (formData :Object | Map, path :string[]) => {
+  const date = getIn(formData, path);
+  if (!isDefined(date)) return formData;
+  const now = DateTime.local();
+  const updatedFormData = setIn(
+    formData,
+    path,
+    DateTime.fromSQL(date.concat(' ', now.toISOTime()))
+  ).toISO();
+  return updatedFormData;
+};
+
+/* eslint-disable import/prefer-default-export */
+export {
+  updateFormWithDateAsDateTime,
+};
