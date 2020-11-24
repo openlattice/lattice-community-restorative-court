dba685067abb6f0b0e6fc2db1b258506ed9abee2
Giulia Campana <giulia@openlattice.com>
2020-11-23 18:29:58 -0800

    create constants for the routes

--- src/containers/app/AppSwitch.js
+++ src/containers/app/AppSwitch.js
@@ -17,11 +17,9 @@ import ProfileContainer from '../profile/src/ProfileContainer';
 import ReferralForm from '../referral/ReferralForm';
 import { APP, REQUEST_STATE } from '../../core/redux/constants';
 import {
-  COMPLETED_REFERRAL,
-  FORM_ID,
-  PEACEMAKER_INFORMATION,
-  PERSON_ID,
-  REFERRAL,
+  COMPLETED_REFERRAL_ROUTE_END,
+  PEACEMAKER_INFORMATION_ROUTE_END,
+  REFERRAL_ROUTE_END,
 } from '../../core/router/Routes';
 import { CenterWrapper } from '../profile/src/styled';
 
@@ -56,11 +54,11 @@ const AppSwitch = ({
   return (
     <Switch>
       <Route
-          path={`${root}/${PERSON_ID}/${COMPLETED_REFERRAL}/${FORM_ID}`}
+          path={`${root}/${COMPLETED_REFERRAL_ROUTE_END}`}
           render={() => <CompletedReferralForm personId={personId} />} />
-      <Route path={`${root}/${PERSON_ID}/${REFERRAL}`} render={() => <ReferralForm personId={personId} />} />
+      <Route path={`${root}/${REFERRAL_ROUTE_END}`} render={() => <ReferralForm personId={personId} />} />
       <Route
-          path={`${root}/${PERSON_ID}/${PEACEMAKER_INFORMATION}`}
+          path={`${root}/${PEACEMAKER_INFORMATION_ROUTE_END}`}
           render={() => <PeacemakerInformationForm personId={personId} />} />
       <Route render={() => <ProfileContainer personId={personId} />} />
     </Switch>
--- src/core/router/Routes.js
+++ src/core/router/Routes.js
@@ -5,17 +5,25 @@
 const ROOT :'/' = '/';
 
 const PERSON_ID :string = ':personId';
-const REFERRAL :string = 'referral';
+
 const PEACEMAKER_INFORMATION :string = 'peacemakerinformation';
+const PEACEMAKER_INFORMATION_ROUTE_END :string = `${PERSON_ID}/${PEACEMAKER_INFORMATION}`;
+
+const REFERRAL :string = 'referral';
+const REFERRAL_ROUTE_END :string = `${PERSON_ID}/${REFERRAL}`;
 
 const FORM_ID :string = ':formId';
 const COMPLETED_REFERRAL :string = 'completedreferral';
+const COMPLETED_REFERRAL_ROUTE_END :string = `${PERSON_ID}/${COMPLETED_REFERRAL}/${FORM_ID}`;
 
 export {
   COMPLETED_REFERRAL,
+  COMPLETED_REFERRAL_ROUTE_END,
   FORM_ID,
-  PERSON_ID,
   PEACEMAKER_INFORMATION,
+  PEACEMAKER_INFORMATION_ROUTE_END,
+  PERSON_ID,
   REFERRAL,
+  REFERRAL_ROUTE_END,
   ROOT,
 };
