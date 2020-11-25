a51e837513adb455080d73d5b4cafe4828320156
Giulia Campana <giulia@openlattice.com>
2020-11-24 18:04:34 -0800

    create a map of form name : select action that is called when user is routed to the blank form

--- src/containers/profile/src/constants/FormConstants.js
+++ src/containers/profile/src/constants/FormConstants.js
@@ -1,4 +1,6 @@
 // @flow
+import { selectReferralForm } from '../../../referral/actions';
+import { selectRepairHarmAgreement } from '../../../repairharm/actions';
 
 // Form Names:
 export const REFERRAL_FORM :string = 'Referral Form';
@@ -6,3 +8,10 @@ export const INTAKE_FORM :string = 'Intake Form';
 export const REPAIR_HARM_AGREEMENT :string = 'Repair Harm Agreement';
 export const RESTITUTION_REFERRAL_FORM :string = 'Restitution Referral Form';
 export const PEACEMAKER_INFORMATION_FORM :string = 'Peacemaker Information Form';
+
+export const SELECT_FORM_ACTIONS_BY_FORM = {
+  [INTAKE_FORM]: () => {},
+  [REFERRAL_FORM]: selectReferralForm,
+  [REPAIR_HARM_AGREEMENT]: selectRepairHarmAgreement,
+  [RESTITUTION_REFERRAL_FORM]: () => {},
+};
