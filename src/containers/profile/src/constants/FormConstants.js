4bd30cebaec1d6651e445aca80eebbb66386ba52
Giulia Campana <giulia@openlattice.com>
2020-11-24 21:07:35 -0800

    update SELECT_FORM_ACTIONS_BY_FORM with selectRestitutionReferral

--- src/containers/profile/src/constants/FormConstants.js
+++ src/containers/profile/src/constants/FormConstants.js
@@ -1,17 +1,18 @@
 // @flow
 import { selectReferralForm } from '../../../referral/actions';
 import { selectRepairHarmAgreement } from '../../../repairharm/actions';
+import { selectRestitutionReferral } from '../../../restitutionreferral/actions';
 
 // Form Names:
 export const REFERRAL_FORM :string = 'Referral Form';
 export const INTAKE_FORM :string = 'Intake Form';
 export const REPAIR_HARM_AGREEMENT :string = 'Repair Harm Agreement';
-export const RESTITUTION_REFERRAL_FORM :string = 'Restitution Referral Form';
+export const RESTITUTION_REFERRAL :string = 'Restitution Referral';
 export const PEACEMAKER_INFORMATION_FORM :string = 'Peacemaker Information Form';
 
 export const SELECT_FORM_ACTIONS_BY_FORM = {
   [INTAKE_FORM]: () => {},
   [REFERRAL_FORM]: selectReferralForm,
   [REPAIR_HARM_AGREEMENT]: selectRepairHarmAgreement,
-  [RESTITUTION_REFERRAL_FORM]: () => {},
+  [RESTITUTION_REFERRAL]: selectRestitutionReferral,
 };
