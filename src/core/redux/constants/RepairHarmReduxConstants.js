67486b086929b8bb40dbacdc47cecbf22a287c35
Giulia Campana <giulia@openlattice.com>
2020-11-24 18:09:09 -0800

    repair harm redux constants

---
+++ src/core/redux/constants/RepairHarmReduxConstants.js
@@ -0,0 +1,9 @@
+// @flow
+
+const REPAIR_HARM :'repairHarm' = 'repairHarm';
+const SELECTED_REPAIR_HARM_AGREEMENT :'selectedRepairHarmAgreement' = 'selectedRepairHarmAgreement';
+
+export {
+  REPAIR_HARM,
+  SELECTED_REPAIR_HARM_AGREEMENT,
+};
