b3812c3f06bb5b7f2a3867fe336772360a10562e
Giulia Campana <giulia@openlattice.com>
2020-12-01 18:11:42 -0800

    update to use WorkerResponse

--- src/containers/repairharm/sagas/submitRepairHarmAgreement.js
+++ src/containers/repairharm/sagas/submitRepairHarmAgreement.js
@@ -7,6 +7,7 @@ import {
 } from '@redux-saga/core/effects';
 import { Logger } from 'lattice-utils';
 import type { Saga } from '@redux-saga/core';
+import type { WorkerResponse } from 'lattice-sagas';
 import type { SequenceAction } from 'redux-reqseq';
 
 import { submitDataGraph } from '../../../core/data/actions';
@@ -15,22 +16,22 @@ import { SUBMIT_REPAIR_HARM_AGREEMENT, submitRepairHarmAgreement } from '../acti
 
 const LOG = new Logger('RepairHarmSagas');
 
-function* submitRepairHarmAgreementWorker(action :SequenceAction) :Saga<*> {
+function* submitRepairHarmAgreementWorker(action :SequenceAction) :Saga<WorkerResponse> {
 
   const { id } = action;
-  const workerResponse = {};
+  let workerResponse :WorkerResponse;
 
   try {
     yield put(submitRepairHarmAgreement.request(id));
     const { value } = action;
 
-    const response :Object = yield call(submitDataGraphWorker, submitDataGraph(value));
+    const response :WorkerResponse = yield call(submitDataGraphWorker, submitDataGraph(value));
     if (response.error) throw response.error;
-
+    workerResponse = { data: response.data };
     yield put(submitRepairHarmAgreement.success(id));
   }
   catch (error) {
-    workerResponse.error = error;
+    workerResponse = { error };
     LOG.error(action.type, error);
     yield put(submitRepairHarmAgreement.failure(id, error));
   }
