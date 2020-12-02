10395047bda72cf1a00aebde6aa62e8c57dc22db
Giulia Campana <giulia@openlattice.com>
2020-12-01 18:05:13 -0800

    add to AddPeopleToCaseForm

--- src/containers/profile/src/caseparticipation/AddPeopleToCaseForm.js
+++ src/containers/profile/src/caseparticipation/AddPeopleToCaseForm.js
@@ -13,18 +13,27 @@ import {
   Select,
   Typography,
 } from 'lattice-ui-kit';
+import { DataUtils } from 'lattice-utils';
 import type { UUID } from 'lattice';
 
 import { CrumbItem, CrumbLink, Crumbs } from '../../../../components/crumbs';
-import { APP_PATHS } from '../../../../core/redux/constants';
+import { PropertyTypes } from '../../../../core/edm/constants';
+import { APP_PATHS, ProfileReduxConstants } from '../../../../core/redux/constants';
 import { selectPerson } from '../../../../core/redux/selectors';
 import { getPersonName } from '../../../../utils/people';
-import { useSelector } from '../../../app/AppProvider';
+import { useDispatch, useSelector } from '../../../app/AppProvider';
+import { searchPeople } from '../actions';
+import { RoleConstants } from '../constants';
+
+const { getEntityKeyId, getPropertyValue } = DataUtils;
+const { NOTES, ROLE } = PropertyTypes;
+const { PERSON_CASE_NEIGHBOR_MAP, PROFILE, SELECTED_CASE } = ProfileReduxConstants;
+const { RESPONDENT } = RoleConstants;
 
 const SearchGrid = styled.div`
   display: grid;
   grid-gap: 20px;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(4, 1fr);
   margin-top: 20px;
   margin-bottom: 20px;
   width: 100%;
@@ -36,6 +45,15 @@ type Props = {
 
 const AddPeopleToCaseForm = ({ personId } :Props) => {
 
+  const personCase :Map = useSelector((store) => store.getIn([PROFILE, SELECTED_CASE]));
+  const caseEKID :?UUID = getEntityKeyId(personCase);
+  const caseNumber = getPropertyValue(personCase, [NOTES, 0]);
+  const caseRoleMap :Map = useSelector((store) => store
+    .getIn([PROFILE, PERSON_CASE_NEIGHBOR_MAP, ROLE, caseEKID], Map()));
+  const respondentPerson = caseRoleMap.getIn([RESPONDENT, 0], Map());
+  const respondentPersonName = getPersonName(respondentPerson);
+  const caseIdentifier = `Case #: ${caseNumber} - ${respondentPersonName}`;
+
   const [formInputs, setFormInputs] = useState({ firstName: '', lastName: '' });
   const [dob, setDOB] = useState('');
 
@@ -44,8 +62,10 @@ const AddPeopleToCaseForm = ({ personId } :Props) => {
     setFormInputs({ ...formInputs, [name]: value });
   };
 
-  const searchPeople = () => {
+  const dispatch = useDispatch();
 
+  const searchPeopleForCase = () => {
+    dispatch(searchPeople({ dob, firstName: formInputs.firstName, lastName: formInputs.lastName }));
   };
 
   const person :Map = useSelector(selectPerson());
@@ -66,20 +86,15 @@ const AddPeopleToCaseForm = ({ personId } :Props) => {
           </CrumbItem>
         </Crumbs>
         <Typography variant="h1">Add People to Case</Typography>
-        <Typography variant="body1">
+        <Typography variant="body1" gutterBottom->
           Search for someone to add as a victim or peacemaker to this case.
         </Typography>
+        <Typography variant="body2">{ caseIdentifier }</Typography>
       </CardSegment>
       <Card>
         <CardSegment padding="30px" vertical>
           <div>Search Participants</div>
           <SearchGrid>
-            <div>
-              <Label>Last name</Label>
-              <Input
-                  name="lastName"
-                  onChange={onInputChange} />
-            </div>
             <div>
               <Label>First name</Label>
               <Input
@@ -87,10 +102,16 @@ const AddPeopleToCaseForm = ({ personId } :Props) => {
                   onChange={onInputChange} />
             </div>
             <div>
+              <Label>Last name</Label>
+              <Input
+                  name="lastName"
+                  onChange={onInputChange} />
+            </div>
+            <div>
               <Label>Date of birth</Label>
               <DatePicker onChange={(date :string) => setDOB(date)} />
             </div>
-            <Button aria-label="Search Button" onClick={searchPeople}>Search</Button>
+            <Button aria-label="Search Button" onClick={searchPeopleForCase}>Search</Button>
           </SearchGrid>
         </CardSegment>
       </Card>
