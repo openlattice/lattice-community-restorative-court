144500f6fc5b6c4e8b7e8855863a819eccb077de
Giulia Campana <giulia@openlattice.com>
2020-12-02 19:28:58 -0800

    new profile redux constants

--- src/core/redux/constants/ProfileReduxConstants.js
+++ src/core/redux/constants/ProfileReduxConstants.js
@@ -6,9 +6,11 @@ const PERSON_CASES :string = 'personCases';
 const PERSON_CASE_NEIGHBOR_MAP :string = 'personCaseNeighborMap';
 const PERSON_NEIGHBOR_MAP :string = 'personNeighborMap';
 const PROFILE :string = 'profile';
+const SEARCHED_PEOPLE :string = 'searchedPeople';
 const SELECTED_CASE :string = 'selectedCase';
 const STAFF_MEMBERS :string = 'staffMembers';
 const STAFF_MEMBER_BY_STATUS_EKID :string = 'staffMemberByStatusEKID';
+const TOTAL_HITS :string = 'totalHits';
 
 export {
   FORM_NEIGHBOR_MAP,
@@ -17,7 +19,9 @@ export {
   PERSON_CASE_NEIGHBOR_MAP,
   PERSON_NEIGHBOR_MAP,
   PROFILE,
+  SEARCHED_PEOPLE,
   SELECTED_CASE,
   STAFF_MEMBERS,
   STAFF_MEMBER_BY_STATUS_EKID,
+  TOTAL_HITS,
 };
