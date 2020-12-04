b2dbc239e2308aa859c42e9b6ee1f005494f199d
Giulia Campana <giulia@openlattice.com>
2020-12-04 00:42:58 -0800

    remove console log

--- src/utils/router/getRelativeRoot.js
+++ src/utils/router/getRelativeRoot.js
@@ -3,7 +3,6 @@ import type { Match } from 'react-router';
 
 const getRelativeRoot = (root :string, match :Match) => {
   if (match.params) {
-    console.log('match.params ', match.params);
     return Object.entries(match.params)
       // $FlowFixMe
       .reduce((newRoot, [param, value]) => newRoot.replace(RegExp(`:${param}`, 'g'), value), root);
