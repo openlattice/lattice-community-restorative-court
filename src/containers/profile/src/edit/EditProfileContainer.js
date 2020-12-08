2875258feb2f23a9b9a05d5cc2a77a1bf25c3678
Giulia Campana <giulia@openlattice.com>
2020-12-08 02:43:33 -0800

    add contact forms to EditProfileContainer

--- src/containers/profile/src/edit/EditProfileContainer.js
+++ src/containers/profile/src/edit/EditProfileContainer.js
@@ -1,13 +1,17 @@
 // @flow
 import React from 'react';
 
+import EditEmailForm from './EditEmailForm';
 import EditPersonDetailsForm from './EditPersonDetailsForm';
 import EditPersonForm from './EditPersonForm';
+import EditPhoneForm from './EditPhoneForm';
 
 const EditProfileContainer = () => (
   <>
     <EditPersonForm />
     <EditPersonDetailsForm />
+    <EditPhoneForm />
+    <EditEmailForm />
   </>
 );
 
