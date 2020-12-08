ef4fe5a68286c078d9c1113bb68ec652b150f586
Giulia Campana <giulia@openlattice.com>
2020-12-08 01:28:59 -0800

    EditPersonForm

---
+++ src/containers/profile/src/edit/EditProfileContainer.js
@@ -0,0 +1,12 @@
+// @flow
+import React from 'react';
+
+import EditPersonForm from './EditPersonForm';
+
+const EditProfileContainer = () => (
+  <>
+    <EditPersonForm />
+  </>
+);
+
+export default EditProfileContainer;
