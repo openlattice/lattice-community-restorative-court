f528e4055c396bd90d1b4d175e15bc560dada755
Giulia Campana <giulia@openlattice.com>
2020-12-17 17:07:23 -0600

    SearchContextConstants

--- src/containers/profile/src/constants/index.js
+++ src/containers/profile/src/constants/index.js
@@ -4,6 +4,7 @@ import * as CaseStatusConstants from './CaseStatusConstants';
 import * as ContactActivityConstants from './ContactActivityConstants';
 import * as FormConstants from './FormConstants';
 import * as RoleConstants from './RoleConstants';
+import * as SearchContextConstants from './SearchContextConstants';
 
 const EMPTY_VALUE = '---';
 
@@ -12,5 +13,6 @@ export {
   ContactActivityConstants,
   FormConstants,
   RoleConstants,
+  SearchContextConstants,
   EMPTY_VALUE
 };
