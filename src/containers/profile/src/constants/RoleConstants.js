94898dd2fb546d5f586ce3c0889e0ce45b7202a8
Giulia Campana <giulia@openlattice.com>
2020-12-17 14:41:39 -0600

    staff member constant

--- src/containers/profile/src/constants/RoleConstants.js
+++ src/containers/profile/src/constants/RoleConstants.js
@@ -3,3 +3,4 @@
 export const PEACEMAKER :string = 'Peacemaker';
 export const RESPONDENT :string = 'Respondent';
 export const VICTIM :string = 'Victim';
+export const STAFF_MEMBER :string = 'Staff Member';
