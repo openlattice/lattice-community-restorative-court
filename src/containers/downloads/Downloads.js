064fe700f7aca8ba0e7f5e5c81cde60a6d930407
Giulia Campana <giulia@openlattice.com>
2021-01-04 10:57:59 -0600

    add DownloadCases to the Downloads screen

--- src/containers/downloads/Downloads.js
+++ src/containers/downloads/Downloads.js
@@ -4,24 +4,28 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { faDownload } from '@fortawesome/pro-light-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 import {
   Card,
   CardSegment,
   CardStack,
   Colors,
 } from 'lattice-ui-kit';
+import type { UUID } from 'lattice';
 import type { Match } from 'react-router';
 
+import DownloadCasesModal from './DownloadCasesModal';
 import DownloadReferralsByAgencyModal from './DownloadReferralsByAgencyModal';
 
-import { APP_PATHS, ReferralReduxConstants } from '../../core/redux/constants';
+import { APP_PATHS, ProfileReduxConstants, ReferralReduxConstants } from '../../core/redux/constants';
 import { useDispatch, useSelector } from '../app/AppProvider';
 import { initializeApplication } from '../app/actions';
+import { getStaff } from '../profile/src/actions';
 import { getAgencies } from '../referral/actions';
 
 const { NEUTRAL } = Colors;
 const { AGENCIES, REFERRAL } = ReferralReduxConstants;
+const { PROFILE, STAFF_MEMBERS } = ProfileReduxConstants;
 
 const DownloadCard = styled(Card)`
   background-color: ${NEUTRAL.N50};
@@ -58,12 +62,15 @@ const Downloads = ({ match, organizationId, root } :Props) => {
   useEffect(() => {
     if (appConfig) {
       dispatch(getAgencies());
+      dispatch(getStaff());
     }
   }, [appConfig, dispatch]);
 
   const [referralsByAgencyModalIsOpen, openReferralsByAgencyModal] = useState(false);
+  const [casesModalIsOpen, openCasesModal] = useState(false);
 
   const agencies :List = useSelector((store) => store.getIn([REFERRAL, AGENCIES], List()));
+  const staff :List = useSelector((store) => store.getIn([PROFILE, STAFF_MEMBERS], List()));
 
   return (
     <CardStack>
@@ -73,10 +80,20 @@ const Downloads = ({ match, organizationId, root } :Props) => {
           <FontAwesomeIcon color={NEUTRAL.N700} icon={faDownload} />
         </DownloadCardSegment>
       </DownloadCard>
+      <DownloadCard onClick={() => openCasesModal(true)}>
+        <DownloadCardSegment padding="8px 16px" vertical={false}>
+          <div>Download Cases Report</div>
+          <FontAwesomeIcon color={NEUTRAL.N700} icon={faDownload} />
+        </DownloadCardSegment>
+      </DownloadCard>
       <DownloadReferralsByAgencyModal
           agencies={agencies}
           isVisible={referralsByAgencyModalIsOpen}
           onClose={() => openReferralsByAgencyModal(false)} />
+      <DownloadCasesModal
+          isVisible={casesModalIsOpen}
+          onClose={() => openCasesModal(false)}
+          staff={staff} />
     </CardStack>
   );
 };
