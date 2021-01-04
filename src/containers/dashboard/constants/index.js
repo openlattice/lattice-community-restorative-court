48835fcf163a586e469bdc65e534a0a0d27d291b
Giulia Campana <giulia@openlattice.com>
2021-01-04 17:58:43 -0600

    add cases stats constants

--- src/containers/dashboard/constants/index.js
+++ src/containers/dashboard/constants/index.js
@@ -8,7 +8,16 @@ const STAFF_CASES_TABLE_HEADERS :string[] = [
   'TOTAL CLOSED CASES'
 ];
 
-/* eslint-disable import/prefer-default-export */
+const CASES_STATS_CONSTANTS = {
+  NO_CONTACT: 'No Contact Made',
+  OPEN_CASES: 'Open Cases',
+  RESPONDENT_DECLINED: 'Respondent Declined',
+  SUCCESSFUL: 'Successful Cases',
+  TERMINATED: 'Terminated Cases',
+  TOTAL_CASES: 'Total Cases',
+};
+
 export {
+  CASES_STATS_CONSTANTS,
   STAFF_CASES_TABLE_HEADERS,
 };
