fb83a92023d785518ac5807b382b921055920549
Giulia Campana <giulia@openlattice.com>
2021-01-04 10:58:12 -0600

    wrap Downloads in AppProvider

--- src/containers/downloads/DownloadsContainer.js
+++ src/containers/downloads/DownloadsContainer.js
@@ -1,25 +1,26 @@
 // @flow
 import React from 'react';
 
+import type { UUID } from 'lattice';
+import type { Match } from 'react-router';
+
 import Downloads from './Downloads';
 
 import AppProvider from '../app/AppProvider';
 
-const DownloadsContainer = (props :Props) => {
-  const {
-    match,
-    organizationId,
-    root,
-  } = props;
-
-  return (
-    <AppProvider>
-      <Downloads
-          match={match}
-          organizationId={organizationId}
-          root={root} />
-    </AppProvider>
-  );
+type Props = {
+  match :Match;
+  organizationId :UUID;
+  root :string;
 };
+
+const DownloadsContainer = ({ match, organizationId, root } :Props) => (
+  <AppProvider>
+    <Downloads
+        match={match}
+        organizationId={organizationId}
+        root={root} />
+  </AppProvider>
+);
 
 export default DownloadsContainer;
