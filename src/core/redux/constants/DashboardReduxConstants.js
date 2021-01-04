bd275f4842432e57f6721333b43ad7a768a25962
Giulia Campana <giulia@openlattice.com>
2021-01-04 17:58:32 -0600

    add cases stats redux const

--- src/core/redux/constants/DashboardReduxConstants.js
+++ src/core/redux/constants/DashboardReduxConstants.js
@@ -1,8 +1,10 @@
 // @flow
+const CASES_STATS :'casesStats' = 'casesStats';
 const DASHBOARD :'dashboard' = 'dashboard';
 const STAFF_CASES_DATA :'staffCasesData' = 'staffCasesData';
 
 export {
+  CASES_STATS,
   DASHBOARD,
   STAFF_CASES_DATA,
 };
