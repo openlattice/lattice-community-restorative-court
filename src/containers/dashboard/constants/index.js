384f72cbcd95861db4bbe2743bdd7ba8df73c359
Giulia Campana <giulia@openlattice.com>
2021-01-04 16:23:16 -0600

    dashboard constants

---
+++ src/containers/dashboard/constants/index.js
@@ -0,0 +1,14 @@
+// @flow
+const STAFF_CASES_TABLE_HEADERS :string[] = [
+  'STAFF',
+  'PENDING INTAKE',
+  'PENDING CIRCLE',
+  'RH AGREEMENT COMPLETED',
+  'TOTAL OPEN CASES',
+  'TOTAL CLOSED CASES'
+];
+
+/* eslint-disable import/prefer-default-export */
+export {
+  STAFF_CASES_TABLE_HEADERS,
+};
