4de4b7cf632cc6148c12027866fb258b331805cc
Giulia Campana <giulia@openlattice.com>
2021-01-04 17:59:02 -0600

    add stats to dashboard

--- src/containers/dashboard/Dashboard.js
+++ src/containers/dashboard/Dashboard.js
@@ -8,13 +8,14 @@ import {
   CardHeader,
   CardSegment,
   Table,
+  Typography,
 } from 'lattice-ui-kit';
 import { ReduxUtils } from 'lattice-utils';
 import type { UUID } from 'lattice';
 import type { Match } from 'react-router';
 
-import { GET_STAFF_CASES_DATA, getStaffCasesData } from './actions';
-import { STAFF_CASES_TABLE_HEADERS } from './constants';
+import { GET_STAFF_CASES_DATA, getCasesStats, getStaffCasesData } from './actions';
+import { CASES_STATS_CONSTANTS, STAFF_CASES_TABLE_HEADERS } from './constants';
 
 import { APP_PATHS, DashboardReduxConstants, REQUEST_STATE } from '../../core/redux/constants';
 import { generateTableHeaders } from '../../utils/table';
@@ -22,9 +23,25 @@ import { useDispatch, useSelector } from '../app/AppProvider';
 import { initializeApplication } from '../app/actions';
 
 const { isPending } = ReduxUtils;
-const { DASHBOARD, STAFF_CASES_DATA } = DashboardReduxConstants;
+const { CASES_STATS, DASHBOARD, STAFF_CASES_DATA } = DashboardReduxConstants;
+const {
+  NO_CONTACT,
+  OPEN_CASES,
+  RESPONDENT_DECLINED,
+  SUCCESSFUL,
+  TERMINATED,
+  TOTAL_CASES,
+} = CASES_STATS_CONSTANTS;
 
 const staffCasesTableHeaders = generateTableHeaders(STAFF_CASES_TABLE_HEADERS);
+
+const StatsGridRow = styled.div`
+  display: grid;
+  grid-gap: 10px 20px;
+  grid-template-columns: repeat(3, 1fr);
+  margin-bottom: 30px;
+  width: 100%;
+`;
 
 const TableHeader = styled(CardHeader)`
   border: none;
@@ -51,22 +68,66 @@ const Dashboard = ({ match, organizationId, root } :Props) => {
   useEffect(() => {
     if (appConfig) {
       dispatch(getStaffCasesData());
+      dispatch(getCasesStats());
     }
   }, [appConfig, dispatch]);
 
+  const casesStats :Map = useSelector((store :Map) => store.getIn([DASHBOARD, CASES_STATS]));
   const staffCasesData :List = useSelector((store :Map) => store.getIn([DASHBOARD, STAFF_CASES_DATA]));
   const fetchRequestState = useSelector((store :Map) => store.getIn([DASHBOARD, GET_STAFF_CASES_DATA, REQUEST_STATE]));
 
   return (
-    <Card>
-      <TableHeader>Staff Cases</TableHeader>
-      <CardSegment padding="0">
-        <Table
-            data={staffCasesData.toJS()}
-            headers={staffCasesTableHeaders}
-            isLoading={isPending(fetchRequestState)} />
-      </CardSegment>
-    </Card>
+    <>
+      <StatsGridRow>
+        <Card>
+          <CardSegment>
+            <Typography variant="h3">{TOTAL_CASES}</Typography>
+            <Typography variant="h1">{casesStats.get(TOTAL_CASES, 0)}</Typography>
+          </CardSegment>
+        </Card>
+        <Card>
+          <CardSegment>
+            <Typography variant="h3">{OPEN_CASES}</Typography>
+            <Typography variant="h1">{casesStats.get(OPEN_CASES, 0)}</Typography>
+          </CardSegment>
+        </Card>
+        <Card>
+          <CardSegment>
+            <Typography variant="h3">{SUCCESSFUL}</Typography>
+            <Typography variant="h1">{casesStats.get(SUCCESSFUL, 0)}</Typography>
+          </CardSegment>
+        </Card>
+      </StatsGridRow>
+      <StatsGridRow>
+        <Card>
+          <CardSegment>
+            <Typography variant="h3">{TERMINATED}</Typography>
+            <Typography variant="h1">{casesStats.get(TERMINATED, 0)}</Typography>
+          </CardSegment>
+        </Card>
+        <Card>
+          <CardSegment>
+            <Typography variant="h3">{RESPONDENT_DECLINED}</Typography>
+            <Typography variant="h1">{casesStats.get(RESPONDENT_DECLINED, 0)}</Typography>
+          </CardSegment>
+        </Card>
+        <Card>
+          <CardSegment>
+            <Typography variant="h3">{NO_CONTACT}</Typography>
+            <Typography variant="h1">{casesStats.get(NO_CONTACT, 0)}</Typography>
+          </CardSegment>
+        </Card>
+      </StatsGridRow>
+      <Card>
+        <TableHeader>Staff Cases</TableHeader>
+        <CardSegment padding="0">
+          <Table
+              data={staffCasesData.toJS()}
+              headers={staffCasesTableHeaders}
+              isLoading={isPending(fetchRequestState)} />
+        </CardSegment>
+      </Card>
+    </>
   );
 };
 
