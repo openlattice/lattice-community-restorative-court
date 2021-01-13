1be34a3bfcbbd28067c97b558aae21e181fb98bc
Giulia Campana <giulia@openlattice.com>
2021-01-13 11:42:28 -0800

    update editPeacemakerInfo saga to update date trained as a datetime

--- src/containers/peacemaker/sagas/editPeacemakerInformation.js
+++ src/containers/peacemaker/sagas/editPeacemakerInformation.js
@@ -5,22 +5,29 @@ import {
   select,
   takeEvery,
 } from '@redux-saga/core/effects';
-import { Map, get } from 'immutable';
+import {
+  Map,
+  get,
+  getIn,
+  setIn,
+} from 'immutable';
 import { LangUtils, Logger } from 'lattice-utils';
+import { DateTime } from 'luxon';
 import type { Saga } from '@redux-saga/core';
 import type { SequenceAction } from 'redux-reqseq';
 
 import { submitPartialReplace } from '../../../core/data/actions';
 import { submitPartialReplaceWorker } from '../../../core/data/sagas';
-import { AppTypes } from '../../../core/edm/constants';
+import { AppTypes, PropertyTypes } from '../../../core/edm/constants';
 import { EDM, PROPERTY_FQNS_BY_TYPE_ID } from '../../../core/redux/constants';
-import { selectEntitySetId } from '../../../core/redux/selectors';
+import { selectEntitySetId, selectPropertyTypeId } from '../../../core/redux/selectors';
 import { formatNewEntityData } from '../../../utils/data';
 import { ERR_ACTION_VALUE_NOT_DEFINED } from '../../../utils/error/constants';
 import { EDIT_PEACEMAKER_INFORMATION, editPeacemakerInformation } from '../actions';
 
 const { isDefined } = LangUtils;
 const { COMMUNICATION, FORM, PERSON_DETAILS } = AppTypes;
+const { GENERAL_DATETIME } = PropertyTypes;
 
 const LOG = new Logger('PeacemakerSagas');
 
@@ -31,12 +38,24 @@ function* editPeacemakerInformationWorker(action :SequenceAction) :Saga<*> {
     yield put(editPeacemakerInformation.request(id));
     if (!isDefined(value)) throw ERR_ACTION_VALUE_NOT_DEFINED;
 
-    const { entityData } = value;
-    const response :Object = yield call(submitPartialReplaceWorker, submitPartialReplace({ entityData }));
+    const { entityData, formEKID } = value;
+    const formESID = yield select(selectEntitySetId(FORM));
+    const generalDateTimePTID = yield select(selectPropertyTypeId(GENERAL_DATETIME));
+    const dateTrained = getIn(entityData, [formESID, formEKID, generalDateTimePTID, 0]);
+    let updatedEntityData = entityData;
+    if (isDefined(dateTrained)) {
+      const now = DateTime.local();
+      const dateTimeTrained = DateTime.fromSQL(dateTrained.concat(' ', now.toISOTime())).toISO();
+
+      updatedEntityData = setIn(updatedEntityData, [formESID, formEKID, generalDateTimePTID, 0], dateTimeTrained);
+    }
+    const response :Object = yield call(
+      submitPartialReplaceWorker,
+      submitPartialReplace({ entityData: updatedEntityData })
+    );
     if (response.error) throw response.error;
 
     const communicationESID = yield select(selectEntitySetId(COMMUNICATION));
-    const formESID = yield select(selectEntitySetId(FORM));
     const personDetailsESID = yield select(selectEntitySetId(PERSON_DETAILS));
 
     const communicationMap = get(entityData, communicationESID);
@@ -44,7 +63,7 @@ function* editPeacemakerInformationWorker(action :SequenceAction) :Saga<*> {
     const personDetailsMap = get(entityData, personDetailsESID);
 
     const communicationEKID = communicationMap ? Object.keys(communicationMap)[0] : undefined;
-    const formEKID = formMap ? Object.keys(formMap)[0] : undefined;
+    // const formEKID = formMap ? Object.keys(formMap)[0] : undefined;
     const personDetailsEKID = personDetailsMap ? Object.keys(personDetailsMap)[0] : undefined;
 
     const communicationData = communicationMap && communicationEKID ? communicationMap[communicationEKID] : undefined;
