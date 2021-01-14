9f5d7154fe75d912a1b77a08c51118a75616966b
Giulia Campana <giulia@openlattice.com>
2021-01-13 18:26:05 -0800

    add date trained and race filters for peacemakers modal

--- src/containers/downloads/DownloadPeacemakersModal.js
+++ src/containers/downloads/DownloadPeacemakersModal.js
@@ -1,10 +1,9 @@
 // @flow
 import React, { useEffect, useState } from 'react';
 
-import { List, Map } from 'immutable';
 import {
   ActionModal,
-  Checkbox,
+  DatePicker,
   Label,
   Select,
 } from 'lattice-ui-kit';
@@ -12,10 +11,10 @@ import { ReduxUtils } from 'lattice-utils';
 import { RequestStates } from 'redux-reqseq';
 
 import { DOWNLOAD_PEACEMAKERS, downloadPeacemakers } from './actions';
+import { DatePickerGrid } from './styled';
 
 import { resetRequestState } from '../../core/redux/actions';
 import { DownloadsReduxConstants, REQUEST_STATE } from '../../core/redux/constants';
-import { getPersonName } from '../../utils/people';
 import { RACES } from '../../utils/people/constants';
 import { useDispatch, useSelector } from '../app/AppProvider';
 
@@ -30,10 +29,16 @@ type Props = {
 const DownloadPeacemakersModal = ({ isVisible, onClose } :Props) => {
 
   const [selectedRace, setRace] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
 
   const dispatch = useDispatch();
   const downloadReport = () => {
-    dispatch(downloadPeacemakers());
+    dispatch(downloadPeacemakers({
+      endDate,
+      selectedRace,
+      startDate,
+    }));
   };
 
   const raceOptions = RACES.map((race :string) => ({ label: race, value: race }));
@@ -42,6 +47,9 @@ const DownloadPeacemakersModal = ({ isVisible, onClose } :Props) => {
 
   useEffect(() => {
     if (isSuccess(downloadRequestState)) {
+      setRace('');
+      setStartDate('');
+      setEndDate('');
       dispatch(resetRequestState([DOWNLOAD_PEACEMAKERS]));
       onClose();
     }
@@ -59,11 +67,17 @@ const DownloadPeacemakersModal = ({ isVisible, onClose } :Props) => {
         textSecondary="Close"
         textTitle="Download Peacemakers"
         viewportScrolling>
+      <Label>Selecting any of the following filters is optional. The default is always to show all values.</Label>
       <Label>Race</Label>
       <Select
           onChange={(option) => setRace(option.value)}
           options={raceOptions}
           placeholder="Select race..." />
+      <Label>Date range for date trained:</Label>
+      <DatePickerGrid>
+        <DatePicker onChange={(date) => setStartDate(date)} />
+        <DatePicker onChange={(date) => setEndDate(date)} />
+      </DatePickerGrid>
     </ActionModal>
   );
 };
