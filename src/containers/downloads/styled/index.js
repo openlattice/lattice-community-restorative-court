2cd0187a57b7cf44aa188927c29f3c09801c72a1
Giulia Campana <giulia@openlattice.com>
2021-01-13 18:25:19 -0800

    DatePickerGrid styled component

---
+++ src/containers/downloads/styled/index.js
@@ -0,0 +1,14 @@
+// @flow
+import styled from 'styled-components';
+
+const DatePickerGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 0 10px;
+  width: 100%;
+`;
+
+/* eslint-disable import/prefer-default-export */
+export {
+  DatePickerGrid,
+};
